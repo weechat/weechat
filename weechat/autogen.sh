04d6257224a7a90ee272b7a359d2f0b25d8faa63
Sebastien Helleu <flashcode@flashtux.org>
2005-04-24 08:48:54 +0000

    Backup and restore Makefile.am before and after gettextize

--- weechat/autogen.sh
+++ weechat/autogen.sh
@@ -2,7 +2,9 @@
 
 # gettextize updates Makefile.am, configure.in
 cp configure.in configure.in.old
+cp Makefile.am Makefile.am.old
 gettextize --copy --force --intl --no-changelog &&
+mv Makefile.am.old Makefile.am
 mv configure.in.old configure.in
 aclocal &&
 # autoheader creates config.h.in needed by autoconf
