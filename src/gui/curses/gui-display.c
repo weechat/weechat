c1b4ccace4858ac2e8a4a39486723bf0e12c58a2
Sebastien Helleu <flashcode@flashtux.org>
2005-12-10 09:28:34 +0000

    Fixed scroll bug (sometimes new lines were not displayed after a scroll up/down)

--- src/gui/curses/gui-display.c
+++ src/gui/curses/gui-display.c
@@ -1675,7 +1675,11 @@ gui_draw_buffer_chat (t_gui_buffer *buffer, int erase)
                 if (!ptr_line && ptr_win->scroll)
                 {
                     if (count == gui_display_line (ptr_win, ptr_win->buffer->last_line, 0, 1))
+                    {
                         ptr_win->scroll = 0;
+                        ptr_win->start_line = NULL;
+                        ptr_win->start_line_pos = 0;
+                    }
                 }
                 
                 if (!ptr_win->scroll && (ptr_win->start_line == ptr_win->buffer->lines))
