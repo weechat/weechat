0da4487a11fd182478de19fb95a99e12d7a68ab9
Sebastien Helleu <flashcode@flashtux.org>
2006-04-12 16:32:24 +0000

    Added --auto option, to remove wait for Return key (useful for automatic CVS build scripts)

--- autogen.sh
+++ autogen.sh
@@ -3,14 +3,18 @@
 # gettextize updates Makefile.am, configure.in
 cp configure.in configure.in.old
 cp Makefile.am Makefile.am.old
-gettextize --copy --force --intl --no-changelog &&
+if test "$1" = "--auto" ; then
+    grep -v 'read dummy < /dev/tty' $(which gettextize) | /bin/sh -s -- --copy --force --intl --no-changelog
+else
+    gettextize --copy --force --intl --no-changelog
+fi
 mv Makefile.am.old Makefile.am
 mv configure.in.old configure.in
-libtoolize --automake --force --copy &&
-aclocal &&
+libtoolize --automake --force --copy
+aclocal
 # autoheader creates config.h.in needed by autoconf
-autoheader &&
+autoheader
 # autoconf creates configure
-autoconf &&
+autoconf
 # automake creates Makefile.in
 automake --add-missing --copy --gnu
