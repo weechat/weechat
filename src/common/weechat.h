f1a39ce7d7e30d56fa8874af49c926b5598b58d2
Sebastien Helleu <flashcode@flashtux.org>
2007-07-02 12:25:13 +0000

    Replaced GPL 2 license by GPL 3

--- src/common/weechat.h
+++ src/common/weechat.h
@@ -4,7 +4,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -13,8 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
@@ -69,7 +68,7 @@
     "\nDeveloped by FlashCode <flashcode@flashtux.org> - " WEECHAT_WEBSITE "\n\n" \
     "This program is free software; you can redistribute it and/or modify\n" \
     "it under the terms of the GNU General Public License as published by\n" \
-    "the Free Software Foundation; either version 2 of the License, or\n" \
+    "the Free Software Foundation; either version 3 of the License, or\n" \
     "(at your option) any later version.\n" \
     "\n", \
     \
@@ -79,8 +78,7 @@
     "GNU General Public License for more details.\n" \
     "\n" \
     "You should have received a copy of the GNU General Public License\n" \
-    "along with this program; if not, write to the Free Software\n" \
-    "Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA\n\n"
+    "along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\n"
     
 /* directory separator, depending on OS */
 
