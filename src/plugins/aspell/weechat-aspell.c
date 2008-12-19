b7a0d258c9e668cd6a51ce455649048729c80655
Sebastien Helleu <flashcode@flashtux.org>
2008-12-19 18:42:16 +0100

    Update doc and translations

--- src/plugins/aspell/weechat-aspell.c
+++ src/plugins/aspell/weechat-aspell.c
@@ -734,13 +734,14 @@ weechat_aspell_modifier_cb (void *data, const char *modifier,
     if (!weechat_aspell_spellers)
         return NULL;
     
+    /* check text search only if option is enabled */
     if (weechat_buffer_get_integer (buffer, "text_search")
         && !weechat_config_boolean (weechat_aspell_config_check_during_search))
         return NULL;
     
     /* for performance: return last stirng built if input string is the
        same (for example user just change cursor position, or input text is
-       refreshed with same content */
+       refreshed with same content) */
     if (aspell_last_modifier_string
         && (strcmp (string, aspell_last_modifier_string) == 0))
     {
