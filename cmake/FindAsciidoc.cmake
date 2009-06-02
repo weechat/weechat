103389822ea3b612298b8fc7c46a338a3773fe05
Sebastien Helleu <flashcode@flashtux.org>
2009-06-02 13:11:16 +0200

    Add cmake detection for asciidoc version

--- cmake/FindAsciidoc.cmake
+++ cmake/FindAsciidoc.cmake
@@ -14,14 +14,30 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-IF(NOT DISABLE_DOC)
+# - Find Asciidoc
+# This module finds if asciidoc (version 8 or newer) is installed.
 
-  FIND_PACKAGE(Asciidoc)
-  IF(ASCIIDOC_FOUND)
-    ADD_SUBDIRECTORY( en )
-    ADD_SUBDIRECTORY( fr )
-  ENDIF(ASCIIDOC_FOUND)
+IF (ASCIIDOC_FOUND)
+  # Already in cache, be silent
+  SET(ASCIIDOC_FIND_QUIETLY TRUE)
+ENDIF (ASCIIDOC_FOUND)
 
-ENDIF(NOT DISABLE_DOC)
+FIND_PROGRAM(
+  ASCIIDOC_EXECUTABLE asciidoc
+  PATHS /bin /usr/bin /usr/local/bin /usr/pkg/bin
+)
 
-INSTALL(FILES ${CMAKE_CURRENT_SOURCE_DIR}/weechat-curses.1 DESTINATION share/man/man1)
+IF(ASCIIDOC_EXECUTABLE)
+  EXECUTE_PROCESS(
+    COMMAND ${ASCIIDOC_EXECUTABLE} --version
+    OUTPUT_VARIABLE ASCIIDOC_VERSION
+    )
+  
+  IF(${ASCIIDOC_VERSION} MATCHES "asciidoc 8.*")
+    SET(ASCIIDOC_FOUND TRUE)
+  ENDIF(${ASCIIDOC_VERSION} MATCHES "asciidoc 8.*")
+  
+  MARK_AS_ADVANCED(
+    ASCIIDOC_EXECUTABLE
+    )
+ENDIF(ASCIIDOC_EXECUTABLE)
