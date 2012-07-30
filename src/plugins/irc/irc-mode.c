048b05cb299fed24341fc41047e4cbc2a72b44e8
Sebastien Helleu <flashcode@flashtux.org>
2012-07-30 14:33:36 +0200

    irc: fix bug with prefix chars which are in chanmodes with a type different from "B" (bug #36996)

--- src/plugins/irc/irc-mode.c
+++ src/plugins/irc/irc-mode.c
@@ -58,6 +58,14 @@ irc_mode_get_chanmode_type (struct t_irc_server *server, char chanmode)
     char chanmode_type, *pos;
     const char *chanmodes, *ptr_chanmodes;
 
+    /*
+     * assume it is type 'B' if mode is in prefix
+     * (we first check that because some exotic servers like irc.webchat.org
+     * include the prefix chars in chanmodes as type 'A', which is wrong)
+     */
+    if (irc_server_get_prefix_mode_index (server, chanmode) >= 0)
+        return 'B';
+
     chanmodes = irc_server_get_chanmodes (server);
     pos = strchr (chanmodes, chanmode);
     if (pos)
@@ -74,10 +82,6 @@ irc_mode_get_chanmode_type (struct t_irc_server *server, char chanmode)
         }
         return chanmode_type;
     }
-
-    /* assume it is type 'B' if mode is not in chanmodes but in prefix */
-    if (irc_server_get_prefix_mode_index (server, chanmode) >= 0)
-        return 'B';
 
     /* unknown mode, type 'D' by default */
     return 'D';
