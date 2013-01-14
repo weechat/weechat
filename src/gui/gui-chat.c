weechat/weechat

commit 70ce7fe3b6caa6128cb6e8bfb0eaaf39f1ebe4ce

core: fix wrapping of words with wide chars (the break was made before the correct position)

src/gui/gui-chat.c
@@ -264,6 +264,9 @@ gui_chat_string_real_pos (const char *string, int pos)
 
 /*
  * gui_chat_get_word_info: returns info about next word: beginning, end, length
+ *                         Note: the word_{start|end}_offset are in bytes, but
+ *                         word_length(_with_spaces) are in number of chars on
+ *                         screen.
  */
 
 void
@@ -274,7 +277,7 @@ gui_chat_get_word_info (struct t_gui_window *window,
 {
     const char *start_data;
     char *next_char, *next_char2;
-    int leading_spaces, char_size;
+    int leading_spaces, char_size_screen;
 
     *word_start_offset = 0;
     *word_end_offset = 0;
@@ -298,10 +301,10 @@ gui_chat_get_word_info (struct t_gui_window *window,
                     if (leading_spaces)
                         *word_start_offset = next_char - start_data;
                     leading_spaces = 0;
-                    char_size = next_char2 - next_char;
                     *word_end_offset = next_char2 - start_data - 1;
-                    (*word_length_with_spaces) += char_size;
-                    (*word_length) += char_size;
+                    char_size_screen = utf8_char_size_screen (next_char);
+                    (*word_length_with_spaces) += char_size_screen;
+                    (*word_length) += char_size_screen;
                 }
                 else
                 {

commit f14f34e9afe8a219cc08f7c86b12780e6adaf1a0

core: fix display bugs with non-printable chars like tabs (part of messages not displayed) (bug #38076)
This bug was introduced by commit 70ce7fe3b6caa6128cb6e8bfb0eaaf39f1ebe4ce.

src/gui/gui-chat.c
@@ -144,6 +144,20 @@ gui_chat_utf_char_valid (const char *utf_char)
 }
 
 /*
+ * Returns number of char needed on screen to display a char.
+ */
+
+int
+gui_chat_char_size_screen (const char *utf_char)
+{
+    /* if char is invalid, it will be displayed as one space on screen */
+    if (!gui_chat_utf_char_valid (utf_char))
+        return 1;
+
+    return utf8_char_size_screen (utf_char);
+}
+
+/*
  * Returns number of char needed on screen to display a word (special chars like
  * colors/attributes are ignored).
  */
@@ -160,7 +174,7 @@ gui_chat_strlen_screen (const char *string)
                                             (unsigned char *)string, 0, 0, 0);
         if (string)
         {
-            size_on_screen = (gui_chat_utf_char_valid (string)) ? utf8_char_size_screen (string) : 1;
+            size_on_screen = gui_chat_char_size_screen (string);
             if (size_on_screen > 0)
                 length += size_on_screen;
             string = utf8_next_char (string);
@@ -208,7 +222,7 @@ gui_chat_string_add_offset_screen (const char *string, int offset_screen)
                                             0, 0, 0);
         if (string)
         {
-            size_on_screen = (gui_chat_utf_char_valid (string)) ? utf8_char_size_screen (string) : 1;
+            size_on_screen = gui_chat_char_size_screen (string);
             offset_screen -= size_on_screen;
             string = utf8_next_char (string);
         }
@@ -240,7 +254,7 @@ gui_chat_string_real_pos (const char *string, int pos)
                                                 0, 0, 0);
         if (ptr_string)
         {
-            size_on_screen = (((unsigned char)ptr_string[0]) < 32) ? 1 : utf8_char_size_screen (ptr_string);
+            size_on_screen = gui_chat_char_size_screen (ptr_string);
             if (size_on_screen > 0)
                 pos -= size_on_screen;
             ptr_string = utf8_next_char (ptr_string);
@@ -293,7 +307,7 @@ gui_chat_get_word_info (struct t_gui_window *window,
                         *word_start_offset = next_char - start_data;
                     leading_spaces = 0;
                     *word_end_offset = next_char2 - start_data - 1;
-                    char_size_screen = utf8_char_size_screen (next_char);
+                    char_size_screen = gui_chat_char_size_screen (next_char);
                     (*word_length_with_spaces) += char_size_screen;
                     (*word_length) += char_size_screen;
                 }