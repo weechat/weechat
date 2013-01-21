1edabb98f5b95a5df924ea04583e39af07f1423d
Sebastien Helleu <flashcode@flashtux.org>
2013-01-21 19:51:45 +0100

    core: remove obsolete/unneeded check of headers and functions in cmake and configure

--- config.h.cmake
+++ config.h.cmake
@@ -1,27 +1,9 @@
-#cmakedefine HAVE_ARPA_INET_H
 #cmakedefine HAVE_LIBINTL_H
-#cmakedefine HAVE_LIMITS_H
-#cmakedefine HAVE_LOCALE_H
-#cmakedefine HAVE_NETDB_H
-#cmakedefine HAVE_NETINET_IN_H
-#cmakedefine HAVE_STDLIB_H
-#cmakedefine HAVE_STRING_H
-#cmakedefine HAVE_SYS_SOCKET_H
-#cmakedefine HAVE_SYS_TIME_H
-#cmakedefine HAVE_SYS_TYPES_H
-#cmakedefine HAVE_UNISTD_H
-#cmakedefine HAVE_PWD_H
-#cmakedefine HAVE_ERRNO_H
-#cmakedefine HAVE_REGEX_H
-#cmakedefine HAVE_WCHAR_H
-#cmakedefine HAVE_SYS_FILE_H
 #cmakedefine HAVE_SYS_RESOURCE_H
 #cmakedefine HAVE_FLOCK
 #cmakedefine HAVE_LANGINFO_CODESET
-#cmakedefine HAVE_STRNDUP
 #cmakedefine HAVE_BACKTRACE
 #cmakedefine ICONV_2ARG_IS_CONST 1
-#cmakedefine HAVE_TCL_CREATE_NS
 #cmakedefine HAVE_MALLINFO
 #cmakedefine HAVE_EAT_NEWLINE_GLITCH
 #define PACKAGE_VERSION "@VERSION@"
