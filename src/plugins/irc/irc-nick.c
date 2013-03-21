19acf8121fc55eff7b74e819a5d24298e3b9a3e0
Sebastien Helleu <flashcode@flashtux.org>
2013-03-21 20:16:49 +0100

    irc: fix prefix color for nick when the prefix is not in irc.color.nick_prefixes: use default color (key "*")
    Problem was happening on a server which has "PREFIX=(Yqaohv)!~&@%+".
    Users with prefix "!" were displayed as lightred (color for "~") instead of
    lightblue (default key "*").
    
    When a prefix was not found, WeeChat was looping on other prefixes (in
    order). Now if color is not found, WeeChat uses immediately the fallback color.

--- src/plugins/irc/irc-nick.c
+++ src/plugins/irc/irc-nick.c
@@ -430,34 +430,30 @@ irc_nick_get_prefix_color_name (struct t_irc_server *server, char prefix)
     static char *default_color = "";
     const char *prefix_modes, *color;
     char mode[2];
-    int i, index;
+    int index;
 
     if (irc_config_hashtable_nick_prefixes)
     {
+        mode[0] = ' ';
+        mode[1] = '\0';
+
         index = irc_server_get_prefix_char_index (server, prefix);
         if (index >= 0)
         {
-            mode[0] = ' ';
-            mode[1] = '\0';
             prefix_modes = irc_server_get_prefix_modes (server);
-            for (i = index; prefix_modes[i]; i++)
-            {
-                mode[0] = prefix_modes[i];
-                color = weechat_hashtable_get (irc_config_hashtable_nick_prefixes,
-                                               mode);
-                if (color)
-                    return color;
-            }
-            /*
-             * no color found with mode (and following modes)?
-             * => fallback to "*"
-             */
-            mode[0] = '*';
+            mode[0] = prefix_modes[index];
             color = weechat_hashtable_get (irc_config_hashtable_nick_prefixes,
                                            mode);
             if (color)
                 return color;
         }
+
+        /* fallback to "*" if no color is found with mode */
+        mode[0] = '*';
+        color = weechat_hashtable_get (irc_config_hashtable_nick_prefixes,
+                                       mode);
+        if (color)
+            return color;
     }
 
     /* no color by default */
