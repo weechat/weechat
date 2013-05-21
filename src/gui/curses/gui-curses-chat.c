weechat/weechat

commit 305175fb8c464a169e767a814f5c400154507e6f

core: fix display of long lines without time (message beginning with two tabs)
The long lines without time were not properly aligned: the second and subsequent
lines should start at column 0 of chat area.

src/gui/curses/gui-curses-chat.c
@@ -478,7 +478,9 @@ gui_chat_display_word (struct t_gui_window *window,
             if ((CONFIG_INTEGER(config_look_align_end_of_lines) == CONFIG_LOOK_ALIGN_END_OF_LINES_MESSAGE)
                 && (CONFIG_INTEGER(config_look_prefix_align) != CONFIG_LOOK_PREFIX_ALIGN_NONE)
                 && CONFIG_STRING(config_look_prefix_suffix)
-                && CONFIG_STRING(config_look_prefix_suffix)[0])
+                && CONFIG_STRING(config_look_prefix_suffix)[0]
+                && line->data->str_time
+                && line->data->str_time[0])
             {
                 if (!simulate)
                 {
@@ -619,7 +621,7 @@ gui_chat_display_time_to_prefix (struct t_gui_window *window,
 
     /* display time */
     if (window->buffer->time_for_each_line
-        && (line->data->str_time && line->data->str_time[0]))
+        && line->data->str_time && line->data->str_time[0])
     {
         if (window->win_chat_cursor_y < window->coords_size)
             window->coords[window->win_chat_cursor_y].time_x1 = window->win_chat_cursor_x;
@@ -1022,8 +1024,8 @@ gui_chat_display_time_to_prefix (struct t_gui_window *window,
         }
 
         if ((CONFIG_INTEGER(config_look_prefix_align) != CONFIG_LOOK_PREFIX_ALIGN_NONE)
-            && (CONFIG_STRING(config_look_prefix_suffix)
-                && CONFIG_STRING(config_look_prefix_suffix)[0]))
+            && CONFIG_STRING(config_look_prefix_suffix)
+            && CONFIG_STRING(config_look_prefix_suffix)[0])
         {
             if (!simulate)
             {

src/gui/gui-line.c
@@ -209,6 +209,10 @@ gui_line_get_align (struct t_gui_buffer *buffer, struct t_gui_line *line,
 {
     int length_time, length_buffer, length_suffix, prefix_length, prefix_is_nick;
 
+    /* return immediately if line has no time (not aligned) */
+    if (!line->data->str_time || !line->data->str_time[0])
+        return 0;
+
     /* return immediately if alignment for end of lines is "time" */
     if (!first_line
         && (CONFIG_INTEGER(config_look_align_end_of_lines) == CONFIG_LOOK_ALIGN_END_OF_LINES_TIME))

commit 2e43bac479d1fba40ba3e3a1e0cc22bcdc9abee7

core: fix line alignment when option weechat.look.buffer_time_format is set to empty string
A regression was introduced by commit 305175fb8c464a169e767a814f5c400154507e6f

src/gui/curses/gui-curses-chat.c
@@ -479,8 +479,7 @@ gui_chat_display_word (struct t_gui_window *window,
                 && (CONFIG_INTEGER(config_look_prefix_align) != CONFIG_LOOK_PREFIX_ALIGN_NONE)
                 && CONFIG_STRING(config_look_prefix_suffix)
                 && CONFIG_STRING(config_look_prefix_suffix)[0]
-                && line->data->str_time
-                && line->data->str_time[0])
+                && line->data->date > 0)
             {
                 if (!simulate)
                 {

src/gui/gui-line.c
@@ -210,7 +210,7 @@ gui_line_get_align (struct t_gui_buffer *buffer, struct t_gui_line *line,
     int length_time, length_buffer, length_suffix, prefix_length, prefix_is_nick;
 
     /* return immediately if line has no time (not aligned) */
-    if (!line->data->str_time || !line->data->str_time[0])
+    if (line->data->date == 0)
         return 0;
 
     /* return immediately if alignment for end of lines is "time" */