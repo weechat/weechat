weechat/weechat

commit 2305c95db08a214dba68e771c9bbabc51ca7d055

irc: fix memory leak when checking the value of ssl_priorities option in servers

src/plugins/irc/irc-config.c
@@ -978,6 +978,7 @@ irc_config_check_gnutls_priorities (const char *priorities)
         return NULL;
 
     rc = gnutls_priority_init (&priority_cache, priorities, &pos_error);
+    gnutls_priority_deinit (priority_cache);
     if (rc == GNUTLS_E_SUCCESS)
         return NULL;
     if (pos_error)

commit 9cc31d88ea989e18ef2cd3ee9b11b242cc134f37

irc: fix crash when setting a wrong value in server option "ssl_priorities"
The bug was introduced by commit 2305c95db08a214dba68e771c9bbabc51ca7d055

src/plugins/irc/irc-config.c
@@ -978,9 +978,11 @@ irc_config_check_gnutls_priorities (const char *priorities)
         return NULL;
 
     rc = gnutls_priority_init (&priority_cache, priorities, &pos_error);
-    gnutls_priority_deinit (priority_cache);
     if (rc == GNUTLS_E_SUCCESS)
+    {
+        gnutls_priority_deinit (priority_cache);
         return NULL;
+    }
     if (pos_error)
         return pos_error;
     return priorities;