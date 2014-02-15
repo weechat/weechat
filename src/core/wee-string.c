360f900d921bae23228b8124477e4d24dc95e190
Sebastien Helleu <flashcode@flashtux.org>
2014-02-15 12:59:05 +0100

    core: move function string_replace before string_replace_regex

--- src/core/wee-string.c
+++ src/core/wee-string.c
@@ -449,64 +449,6 @@ string_match (const char *string, const char *mask, int case_sensitive)
 }
 
 /*
- * Replaces a string by new one in a string.
- *
- * Note: result must be freed after use.
- */
-
-char *
-string_replace (const char *string, const char *search, const char *replace)
-{
-    const char *pos;
-    char *new_string;
-    int length1, length2, length_new, count;
-
-    if (!string || !search || !replace)
-        return NULL;
-
-    length1 = strlen (search);
-    length2 = strlen (replace);
-
-    /* count number of strings to replace */
-    count = 0;
-    pos = string;
-    while (pos && pos[0] && (pos = strstr (pos, search)))
-    {
-        count++;
-        pos += length1;
-    }
-
-    /* easy: no string to replace! */
-    if (count == 0)
-        return strdup (string);
-
-    /* compute needed memory for new string */
-    length_new = strlen (string) - (count * length1) + (count * length2) + 1;
-
-    /* allocate new string */
-    new_string = malloc (length_new);
-    if (!new_string)
-        return strdup (string);
-
-    /* replace all occurrences */
-    new_string[0] = '\0';
-    while (string && string[0])
-    {
-        pos = strstr (string, search);
-        if (pos)
-        {
-            strncat (new_string, string, pos - string);
-            strcat (new_string, replace);
-            pos += length1;
-        }
-        else
-            strcat (new_string, string);
-        string = pos;
-    }
-    return new_string;
-}
-
-/*
  * Expands home in a path.
  *
  * Example: "~/file.txt" => "/home/xxx/file.txt"
@@ -1142,6 +1084,64 @@ string_has_highlight_regex (const char *string, const char *regex)
     regfree (&reg);
 
     return rc;
+}
+
+/*
+ * Replaces a string by new one in a string.
+ *
+ * Note: result must be freed after use.
+ */
+
+char *
+string_replace (const char *string, const char *search, const char *replace)
+{
+    const char *pos;
+    char *new_string;
+    int length1, length2, length_new, count;
+
+    if (!string || !search || !replace)
+        return NULL;
+
+    length1 = strlen (search);
+    length2 = strlen (replace);
+
+    /* count number of strings to replace */
+    count = 0;
+    pos = string;
+    while (pos && pos[0] && (pos = strstr (pos, search)))
+    {
+        count++;
+        pos += length1;
+    }
+
+    /* easy: no string to replace! */
+    if (count == 0)
+        return strdup (string);
+
+    /* compute needed memory for new string */
+    length_new = strlen (string) - (count * length1) + (count * length2) + 1;
+
+    /* allocate new string */
+    new_string = malloc (length_new);
+    if (!new_string)
+        return strdup (string);
+
+    /* replace all occurrences */
+    new_string[0] = '\0';
+    while (string && string[0])
+    {
+        pos = strstr (string, search);
+        if (pos)
+        {
+            strncat (new_string, string, pos - string);
+            strcat (new_string, replace);
+            pos += length1;
+        }
+        else
+            strcat (new_string, string);
+        string = pos;
+    }
+    return new_string;
 }
 
 /*
