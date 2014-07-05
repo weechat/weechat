e0e3f9fdeecc438489c06eb72e7afe91de3c7102
Sébastien Helleu <flashcode@flashtux.org>
2014-07-05 10:17:28 +0200

    core: check that regex is not NULL in function string_regcomp

--- src/core/wee-string.c
+++ src/core/wee-string.c
@@ -922,6 +922,9 @@ string_regcomp (void *preg, const char *regex, int default_flags)
     const char *ptr_regex;
     int flags;
 
+    if (!regex)
+        return -1;
+
     ptr_regex = string_regex_flags (regex, default_flags, &flags);
     return regcomp ((regex_t *)preg, ptr_regex, flags);
 }
