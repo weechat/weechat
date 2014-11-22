81e337645c51ab3b842e387c9d7e40b826ff352f
Sébastien Helleu <flashcode@flashtux.org>
2014-11-22 11:23:01 +0100

    core: fix Lua detection on FreeBSD with cmake

--- cmake/FindLua.cmake
+++ cmake/FindLua.cmake
@@ -35,5 +35,5 @@ endif()
 
 find_package(PkgConfig)
 if(PKG_CONFIG_FOUND)
-  pkg_search_module(LUA lua5.2 lua52 lua5.1 lua51 lua5.0 lua50 lua)
+  pkg_search_module(LUA lua5.2 lua-5.2 lua52 lua5.1 lua-5.1 lua51 lua-5.0 lua5.0 lua50 lua)
 endif()
