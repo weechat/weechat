9e8a6d1e20fab918e0915ca5a8ee7e3c757ab6f2
Sébastien Helleu <flashcode@flashtux.org>
2014-12-01 07:50:11 +0100

    script: rename callback for infolist "script_script"

--- src/plugins/script/script-info.c
+++ src/plugins/script/script-info.c
@@ -29,60 +29,55 @@
 
 
 /*
- * Returns infolist with script info.
+ * Returns script infolist "script_script".
  */
 
 struct t_infolist *
-script_info_get_infolist_cb (void *data, const char *infolist_name,
-                             void *pointer, const char *arguments)
+script_info_infolist_script_script_cb (void *data, const char *infolist_name,
+                                       void *pointer, const char *arguments)
 {
     struct t_infolist *ptr_infolist;
     struct t_script_repo *ptr_script;
 
     /* make C compiler happy */
     (void) data;
+    (void) infolist_name;
 
-    if (!infolist_name || !infolist_name[0])
+    if (pointer && !script_repo_script_valid (pointer))
         return NULL;
 
-    if (weechat_strcasecmp (infolist_name, "script_script") == 0)
-    {
-        if (pointer && !script_repo_script_valid (pointer))
-            return NULL;
+    ptr_infolist = weechat_infolist_new ();
+    if (!ptr_infolist)
+        return NULL;
 
-        ptr_infolist = weechat_infolist_new ();
-        if (ptr_infolist)
+    if (pointer)
+    {
+        /* build list with only one script */
+        if (!script_repo_add_to_infolist (ptr_infolist, pointer))
         {
-            if (pointer)
+            weechat_infolist_free (ptr_infolist);
+            return NULL;
+        }
+        return ptr_infolist;
+    }
+    else
+    {
+        /* build list with all scripts matching arguments */
+        for (ptr_script = scripts_repo; ptr_script;
+             ptr_script = ptr_script->next_script)
+        {
+            if (!arguments || !arguments[0]
+                || weechat_string_match (ptr_script->name_with_extension,
+                                         arguments, 0))
             {
-                /* build list with only one script */
-                if (!script_repo_add_to_infolist (ptr_infolist, pointer))
+                if (!script_repo_add_to_infolist (ptr_infolist, ptr_script))
                 {
                     weechat_infolist_free (ptr_infolist);
                     return NULL;
                 }
-                return ptr_infolist;
-            }
-            else
-            {
-                /* build list with all scripts matching arguments */
-                for (ptr_script = scripts_repo; ptr_script;
-                     ptr_script = ptr_script->next_script)
-                {
-                    if (!arguments || !arguments[0]
-                        || weechat_string_match (ptr_script->name_with_extension,
-                                                 arguments, 0))
-                    {
-                        if (!script_repo_add_to_infolist (ptr_infolist, ptr_script))
-                        {
-                            weechat_infolist_free (ptr_infolist);
-                            return NULL;
-                        }
-                    }
-                }
-                return ptr_infolist;
             }
         }
+        return ptr_infolist;
     }
 
     return NULL;
@@ -96,14 +91,16 @@ void
 script_info_init ()
 {
     /* infolist hooks */
-    weechat_hook_infolist ("script_script",
-                           N_("list of scripts"),
-                           N_("script pointer (optional)"),
-                           N_("script name with extension "
-                              "(wildcard \"*\" is allowed) (optional)"),
-                           &script_info_get_infolist_cb, NULL);
+    weechat_hook_infolist (
+        "script_script",
+        N_("list of scripts"),
+        N_("script pointer (optional)"),
+        N_("script name with extension "
+           "(wildcard \"*\" is allowed) (optional)"),
+        &script_info_infolist_script_script_cb, NULL);
 
     /* hdata hooks */
-    weechat_hook_hdata ("script_script", N_("scripts from repository"),
-                        &script_repo_hdata_script_cb, NULL);
+    weechat_hook_hdata (
+        "script_script", N_("scripts from repository"),
+        &script_repo_hdata_script_cb, NULL);
 }
