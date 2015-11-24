4fca6c58c8d817363a20da6691c1e9705f6234f1
Sébastien Helleu <flashcode@flashtux.org>
2015-11-24 20:26:06 +0100

    irc: add a condition for colon before the password in PASS message (issue #602)
    Some weird IRC servers may refuse a PASS command with a colon, so we
    send a colon only if the password contains spaces or begins with a
    colon.

--- src/plugins/irc/irc-server.c
+++ src/plugins/irc/irc-server.c
@@ -3328,7 +3328,13 @@ irc_server_login (struct t_irc_server *server)
         server, IRC_SERVER_OPTION_CAPABILITIES);
 
     if (password && password[0])
-        irc_server_sendf (server, 0, NULL, "PASS :%s", password);
+    {
+        irc_server_sendf (
+            server, 0, NULL,
+            "PASS %s%s",
+            ((password[0] == ':') || (strchr (password, ' '))) ? ":" : "",
+            password);
+    }
 
     if (!server->nick)
     {
