0e314ca7df2f054086f7e14a87dd55ddc29a64cf
Simmo Saan <simmo.saan@gmail.com>
2015-12-20 17:28:43 +0200

    plugins: add info to get regex engine WeeChat was compiled with

--- src/plugins/plugin-api.c
+++ src/plugins/plugin-api.c
@@ -431,6 +431,28 @@ plugin_api_info_dir_separator_cb (const void *pointer, void *data,
 }
 
 /*
+ * Returns WeeChat info "regex_engine".
+ */
+
+const char *
+plugin_api_info_regex_engine_cb (const void *pointer, void *data,
+                                 const char *info_name,
+                                 const char *arguments)
+{
+    /* make C compiler happy */
+    (void) pointer;
+    (void) data;
+    (void) info_name;
+    (void) arguments;
+
+#ifdef HAVE_PCRE
+    return "pcre";
+#else
+    return "posix";
+#endif
+}
+
+/*
  * Returns WeeChat info "weechat_dir".
  */
 
@@ -1913,6 +1935,9 @@ plugin_api_init ()
     hook_info (NULL, "dir_separator",
                N_("directory separator"),
                NULL, &plugin_api_info_dir_separator_cb, NULL, NULL);
+    hook_info (NULL, "regex_engine",
+               N_("WeeChat regular expression engine"),
+               NULL, &plugin_api_info_regex_engine_cb, NULL, NULL);
     hook_info (NULL, "weechat_dir",
                N_("WeeChat directory"),
                NULL, &plugin_api_info_weechat_dir_cb, NULL, NULL);
