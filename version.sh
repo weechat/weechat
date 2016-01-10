241acecdbc405f4b657ca22ab68dce6f841e157e
Sébastien Helleu <flashcode@flashtux.org>
2016-01-10 10:31:35 +0100

    Version 1.5-dev

--- version.sh
+++ version.sh
@@ -33,8 +33,8 @@
 #
 
 WEECHAT_STABLE=1.4
-WEECHAT_DEVEL=1.4
-WEECHAT_DEVEL_FULL=1.4
+WEECHAT_DEVEL=1.5
+WEECHAT_DEVEL_FULL=1.5-dev
 
 if [ $# -lt 1 ]; then
     echo >&2 "Syntax: $0 stable|devel|devel-full|devel-major|devel-minor|devel-patch"
