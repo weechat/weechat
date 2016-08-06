399636f98453bdc1fca19b4dbc742993549aa6c3
Sébastien Helleu <flashcode@flashtux.org>
2016-08-06 22:13:52 +0200

    api: fix connection to servers with hook_connect() on Windows 10 with Windows subsystem for Linux (closes #770)

--- src/core/wee-hook.h
+++ src/core/wee-hook.h
@@ -27,7 +27,7 @@
 #include <gnutls/gnutls.h>
 #endif
 
-#if defined(__CYGWIN__) || defined(__APPLE__) || defined(__MACH__)
+#if !defined(HAVE_SOCKETPAIR_SOCK_DGRAM) || defined(__CYGWIN__) || defined(__APPLE__) || defined(__MACH__)
 /*
  * For the connect hook, when this is defined an array of sockets will
  * be passed from the parent process to the child process instead of using
