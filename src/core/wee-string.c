weechat/weechat

commit af138840b3484e106d247c4a6a18a28e8225f5bb

api: return pointer to string in function string_dyn_free() if argument "free_string" is 0

src/core/wee-string.c
@@ -3433,22 +3433,34 @@ string_dyn_concat (char **string, const char *add)
  * If free_string == 1, the string itself is freed in the structure.
  * Otherwise the pointer (*string) remains valid after this call, and
  * the caller must manually free the string with a call to free().
+ *
+ * Returns the pointer to the string if "free_string" is 0 (string
+ * pointer is still valid), or NULL if "free_string" is 1 (string
+ * has been freed).
  */
 
-void
+char *
 string_dyn_free (char **string, int free_string)
 {
     struct t_string_dyn *ptr_string_dyn;
+    char *ptr_string;
 
     if (!string || !*string)
-        return;
+        return NULL;
 
     ptr_string_dyn = (struct t_string_dyn *)string;
 
     if (free_string)
+    {
         free (ptr_string_dyn->string);
+        return NULL;
+    }
+
+    ptr_string = ptr_string_dyn->string;
 
     free (ptr_string_dyn);
+
+    return ptr_string;
 }
 
 /*

src/core/wee-string.h
@@ -121,7 +121,7 @@ extern void string_shared_free (const char *string);
 extern char **string_dyn_alloc (int size_alloc);
 extern int string_dyn_copy (char **string, const char *new_string);
 extern int string_dyn_concat (char **string, const char *add);
-extern void string_dyn_free (char **string, int free_string);
+extern char *string_dyn_free (char **string, int free_string);
 extern void string_end ();
 
 #endif /* WEECHAT_STRING_H */

src/plugins/weechat-plugin.h
@@ -59,7 +59,7 @@ struct timeval;
  * please change the date with current one; for a second change at same
  * date, increment the 01, otherwise please keep 01.
  */
-#define WEECHAT_PLUGIN_API_VERSION "20170312-01"
+#define WEECHAT_PLUGIN_API_VERSION "20170401-01"
 
 /* macros for defining plugin infos */
 #define WEECHAT_PLUGIN_NAME(__name)                                     \
@@ -328,7 +328,7 @@ struct t_weechat_plugin
     char **(*string_dyn_alloc) (int size_alloc);
     int (*string_dyn_copy) (char **string, const char *new_string);
     int (*string_dyn_concat) (char **string, const char *add);
-    void (*string_dyn_free) (char **string, int free_string);
+    char *(*string_dyn_free) (char **string, int free_string);
 
     /* UTF-8 strings */
     int (*utf8_has_8bits) (const char *string);

commit c652598ad7be3fb3a3ff7287eb0ea5cfb712817f

api: fix memory leak in function string_dyn_free()
Bug was introduced by commit af138840b3484e106d247c4a6a18a28e8225f5bb.

src/core/wee-string.c
@@ -3453,10 +3453,12 @@ string_dyn_free (char **string, int free_string)
     if (free_string)
     {
         free (ptr_string_dyn->string);
-        return NULL;
+        ptr_string = NULL;
     }
-
-    ptr_string = ptr_string_dyn->string;
+    else
+    {
+        ptr_string = ptr_string_dyn->string;
+    }
 
     free (ptr_string_dyn);
 