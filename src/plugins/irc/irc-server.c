383652aa2a6e38ad62ec03f02cbcda9ab13cf7d8
Sébastien Helleu <flashcode@flashtux.org>
2017-06-14 07:29:11 +0200

    irc: remove unused functions irc_server_get_number_connected and irc_server_get_number_buffer (issue #1012)

--- src/plugins/irc/irc-server.c
+++ src/plugins/irc/irc-server.c
@@ -5045,50 +5045,6 @@ irc_server_autojoin_channels (struct t_irc_server *server)
 }
 
 /*
- * Returns number of connected servers.
- */
-
-int
-irc_server_get_number_connected ()
-{
-    struct t_irc_server *ptr_server;
-    int number;
-
-    number = 0;
-    for (ptr_server = irc_servers; ptr_server;
-         ptr_server = ptr_server->next_server)
-    {
-        if (ptr_server->is_connected)
-            number++;
-    }
-    return number;
-}
-
-/*
- * Returns position of a server and total number of servers with a buffer.
- */
-
-void
-irc_server_get_number_buffer (struct t_irc_server *server,
-                              int *server_pos, int *server_total)
-{
-    struct t_irc_server *ptr_server;
-
-    *server_pos = 0;
-    *server_total = 0;
-    for (ptr_server = irc_servers; ptr_server;
-         ptr_server = ptr_server->next_server)
-    {
-        if (ptr_server->buffer)
-        {
-            (*server_total)++;
-            if (ptr_server == server)
-                *server_pos = *server_total;
-        }
-    }
-}
-
-/*
  * Returns number of channels for server.
  */
 
