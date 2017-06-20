weechat/weechat

commit fadee506642ee80f42027816cc6cb758dcb68011

irc: don't reset nick properties (prefixes/away/account/realname) on /names when the nick already exists (closes #1019)

src/plugins/irc/irc-nick.c
@@ -466,25 +466,7 @@ irc_nick_new (struct t_irc_server *server, struct t_irc_channel *channel,
     /* nick already exists on this channel? */
     ptr_nick = irc_nick_search (server, channel, nickname);
     if (ptr_nick)
-    {
-        /* remove old nick from nicklist */
-        irc_nick_nicklist_remove (server, channel, ptr_nick);
-
-        /* update nick */
-        irc_nick_set_prefixes (server, ptr_nick, prefixes);
-        ptr_nick->away = away;
-        if (ptr_nick->account)
-            free (ptr_nick->account);
-        ptr_nick->account = (account) ? strdup (account) : NULL;
-        if (ptr_nick->realname)
-            free (ptr_nick->realname);
-        ptr_nick->realname = (realname) ? strdup (realname) : NULL;
-
-        /* add new nick in nicklist */
-        irc_nick_nicklist_add (server, channel, ptr_nick);
-
         return ptr_nick;
-    }
 
     /* alloc memory for new nick */
     if ((new_nick = malloc (sizeof (*new_nick))) == NULL)

commit c15c30770ebdc4dd2abfb9aa9e6feafa1d7ba2d3

irc: fix update of nick prefixes when the names are received in message 353 (closes #1022)
This regression was introduced by commit
fadee506642ee80f42027816cc6cb758dcb68011 (issue #1019)

src/plugins/irc/irc-nick.c
@@ -466,7 +466,18 @@ irc_nick_new (struct t_irc_server *server, struct t_irc_channel *channel,
     /* nick already exists on this channel? */
     ptr_nick = irc_nick_search (server, channel, nickname);
     if (ptr_nick)
+    {
+        /* remove old nick from nicklist */
+        irc_nick_nicklist_remove (server, channel, ptr_nick);
+
+        /* update nick prefixes */
+        irc_nick_set_prefixes (server, ptr_nick, prefixes);
+
+        /* add new nick in nicklist */
+        irc_nick_nicklist_add (server, channel, ptr_nick);
+
         return ptr_nick;
+    }
 
     /* alloc memory for new nick */
     if ((new_nick = malloc (sizeof (*new_nick))) == NULL)