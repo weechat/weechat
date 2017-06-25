e579e8692936d232ded2fe07ff7aaf8b22b06a2d
Sébastien Helleu <flashcode@flashtux.org>
2017-06-25 21:22:36 +0200

    fset: shift plugins priority to insert fset plugin between buflist and fifo

--- src/plugins/exec/exec.c
+++ src/plugins/exec/exec.c
@@ -37,7 +37,7 @@ WEECHAT_PLUGIN_DESCRIPTION(N_("Execution of external commands in WeeChat"));
 WEECHAT_PLUGIN_AUTHOR("Sébastien Helleu <flashcode@flashtux.org>");
 WEECHAT_PLUGIN_VERSION(WEECHAT_VERSION);
 WEECHAT_PLUGIN_LICENSE(WEECHAT_LICENSE);
-WEECHAT_PLUGIN_PRIORITY(12000);
+WEECHAT_PLUGIN_PRIORITY(13000);
 
 struct t_weechat_plugin *weechat_exec_plugin = NULL;
 
--- src/plugins/logger/logger.c
+++ src/plugins/logger/logger.c
@@ -50,7 +50,7 @@ WEECHAT_PLUGIN_DESCRIPTION(N_("Log buffers to files"));
 WEECHAT_PLUGIN_AUTHOR("Sébastien Helleu <flashcode@flashtux.org>");
 WEECHAT_PLUGIN_VERSION(WEECHAT_VERSION);
 WEECHAT_PLUGIN_LICENSE(WEECHAT_LICENSE);
-WEECHAT_PLUGIN_PRIORITY(13000);
+WEECHAT_PLUGIN_PRIORITY(14000);
 
 struct t_weechat_plugin *weechat_logger_plugin = NULL;
 
