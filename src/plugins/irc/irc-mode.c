weechat/weechat

commit b38b2abe3b397af1133951fb5326cc5cbb80807e

irc: fix parsing of MODE, 341 (invite) and CHGHOST commands when there are colons (closes #1296)

src/plugins/irc/irc-protocol.c
@@ -767,12 +767,13 @@ IRC_PROTOCOL_CALLBACK(cap)
  *   :nick!user@host CHGHOST user new.host.goes.here
  *   :nick!user@host CHGHOST newuser host
  *   :nick!user@host CHGHOST newuser new.host.goes.here
+ *   :nick!user@host CHGHOST newuser :new.host.goes.here
  */
 
 IRC_PROTOCOL_CALLBACK(chghost)
 {
     int length, local_chghost, smart_filter;
-    char *str_host;
+    char *str_host, *pos_new_host;
     struct t_irc_channel *ptr_channel;
     struct t_irc_nick *ptr_nick;
     struct t_irc_channel_speaking *ptr_nick_speaking;
@@ -782,7 +783,9 @@ IRC_PROTOCOL_CALLBACK(chghost)
 
     local_chghost = (irc_server_strcasecmp (server, nick, server->nick) == 0);
 
-    length = strlen (argv[2]) + 1 + strlen (argv[3]) + 1;
+    pos_new_host = (argv_eol[3][0] == ':') ? argv_eol[3] + 1 : argv_eol[3];
+
+    length = strlen (argv[2]) + 1 + strlen (pos_new_host) + 1;
     str_host = malloc (length);
     if (!str_host)
     {
@@ -793,7 +796,7 @@ IRC_PROTOCOL_CALLBACK(chghost)
         return WEECHAT_RC_OK;
     }
 
-    snprintf (str_host, length, "%s@%s", argv[2], argv[3]);
+    snprintf (str_host, length, "%s@%s", argv[2], pos_new_host);
     for (ptr_channel = server->channels; ptr_channel;
          ptr_channel = ptr_channel->next_channel)
     {
@@ -1380,11 +1383,12 @@ IRC_PROTOCOL_CALLBACK(kill)
  *
  * Message looks like:
  *   :nick!user@host MODE #test +o nick
+ *   :nick!user@host MODE #test :+o :nick
  */
 
 IRC_PROTOCOL_CALLBACK(mode)
 {
-    char *pos_modes;
+    char *pos_modes, *pos_modes_args;
     int smart_filter, local_mode;
     struct t_irc_channel *ptr_channel;
     struct t_irc_nick *ptr_nick;
@@ -1393,7 +1397,10 @@ IRC_PROTOCOL_CALLBACK(mode)
     IRC_PROTOCOL_MIN_ARGS(4);
     IRC_PROTOCOL_CHECK_HOST;
 
-    pos_modes = (argv_eol[3][0] == ':') ? argv_eol[3] + 1 : argv_eol[3];
+    pos_modes = (argv[3][0] == ':') ? argv[3] + 1 : argv[3];
+    pos_modes_args = NULL;
+    if (argc > 4)
+        pos_modes_args = (argv_eol[4][0] == ':') ? argv_eol[4] + 1 : argv_eol[4];
 
     if (irc_channel_is_channel (server, argv[2]))
     {
@@ -1415,13 +1422,15 @@ IRC_PROTOCOL_CALLBACK(mode)
                                (smart_filter && !local_mode) ?
                                "irc_smart_filter" : NULL,
                                NULL, address),
-            _("%sMode %s%s %s[%s%s%s]%s by %s%s"),
+            _("%sMode %s%s %s[%s%s%s%s%s]%s by %s%s"),
             weechat_prefix ("network"),
             IRC_COLOR_CHAT_CHANNEL,
             (ptr_channel) ? ptr_channel->name : argv[2],
             IRC_COLOR_CHAT_DELIMITERS,
             IRC_COLOR_RESET,
             pos_modes,
+            (pos_modes_args) ? " " : "",
+            (pos_modes_args) ? pos_modes_args : "",
             IRC_COLOR_CHAT_DELIMITERS,
             IRC_COLOR_RESET,
             irc_nick_color_for_msg (server, 1, ptr_nick, nick),
@@ -4032,11 +4041,16 @@ IRC_PROTOCOL_CALLBACK(338)
  *
  * Message looks like:
  *   :server 341 mynick nick #channel
+ *   :server 341 mynick nick :#channel
  */
 
 IRC_PROTOCOL_CALLBACK(341)
 {
+    char *pos_channel;
+
     IRC_PROTOCOL_MIN_ARGS(5);
+
+    pos_channel = (argv[4][0] == ':') ? argv[4] + 1 : argv[4];
 
     weechat_printf_date_tags (
         irc_msgbuffer_get_target_buffer (server, argv[2], command, NULL, NULL),
@@ -4051,7 +4065,7 @@ IRC_PROTOCOL_CALLBACK(341)
         argv[3],
         IRC_COLOR_RESET,
         IRC_COLOR_CHAT_CHANNEL,
-        argv[4],
+        pos_channel,
         IRC_COLOR_RESET);
 
     return WEECHAT_RC_OK;

po/cs.po
@@ -21,8 +21,8 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2019-01-26 09:58+0100\n"
-"PO-Revision-Date: 2018-11-17 10:36+0100\n"
+"POT-Creation-Date: 2019-01-29 17:06+0100\n"
+"PO-Revision-Date: 2019-01-29 21:01+0100\n"
 "Last-Translator: Ondřej Súkup <mimi.vx@gmail.com>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
 "Language: cs\n"
@@ -9139,8 +9139,8 @@ msgid "%s%sYou were killed by %s%s%s"
 msgstr "%s%sByl jste zabit uživatelem %s%s%s"
 
 #, c-format
-msgid "%sMode %s%s %s[%s%s%s]%s by %s%s"
-msgstr "%sMód %s%s %s[%s%s%s]%s od %s%s"
+msgid "%sMode %s%s %s[%s%s%s%s%s]%s by %s%s"
+msgstr "%sMód %s%s %s[%s%s%s%s%s]%s od %s%s"
 
 #, c-format
 msgid "%sUser mode %s[%s%s%s]%s by %s%s"

po/de.po
@@ -24,8 +24,8 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2019-01-26 09:58+0100\n"
-"PO-Revision-Date: 2019-01-26 10:05+0100\n"
+"POT-Creation-Date: 2019-01-29 17:06+0100\n"
+"PO-Revision-Date: 2019-01-29 21:02+0100\n"
 "Last-Translator: Nils Görs <weechatter@arcor.de>\n"
 "Language-Team: German <kde-i18n-de@kde.org>\n"
 "Language: de\n"
@@ -10932,8 +10932,8 @@ msgid "%s%sYou were killed by %s%s%s"
 msgstr "%s%sDu wurdest durch %s%s%s gekillt"
 
 #, c-format
-msgid "%sMode %s%s %s[%s%s%s]%s by %s%s"
-msgstr "%sModus %s%s %s[%s%s%s]%s durch %s%s"
+msgid "%sMode %s%s %s[%s%s%s%s%s]%s by %s%s"
+msgstr "%sModus %s%s %s[%s%s%s%s%s]%s durch %s%s"
 
 #, c-format
 msgid "%sUser mode %s[%s%s%s]%s by %s%s"

po/es.po
@@ -22,8 +22,8 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2019-01-26 09:58+0100\n"
-"PO-Revision-Date: 2018-11-17 10:36+0100\n"
+"POT-Creation-Date: 2019-01-29 17:06+0100\n"
+"PO-Revision-Date: 2019-01-29 21:02+0100\n"
 "Last-Translator: Elián Hanisch <lambdae2@gmail.com>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
 "Language: es\n"
@@ -9462,8 +9462,8 @@ msgid "%s%sYou were killed by %s%s%s"
 msgstr "%s%sFuiste desconectado por %s%s%s"
 
 #, c-format
-msgid "%sMode %s%s %s[%s%s%s]%s by %s%s"
-msgstr "%sModo %s%s %s[%s%s%s]%s por %s%s"
+msgid "%sMode %s%s %s[%s%s%s%s%s]%s by %s%s"
+msgstr "%sModo %s%s %s[%s%s%s%s%s]%s por %s%s"
 
 #, c-format
 msgid "%sUser mode %s[%s%s%s]%s by %s%s"

po/fr.po
@@ -21,8 +21,8 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2019-01-26 09:58+0100\n"
-"PO-Revision-Date: 2019-01-26 10:05+0100\n"
+"POT-Creation-Date: 2019-01-29 17:06+0100\n"
+"PO-Revision-Date: 2019-01-29 21:01+0100\n"
 "Last-Translator: Sébastien Helleu <flashcode@flashtux.org>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
 "Language: fr\n"
@@ -10676,8 +10676,8 @@ msgid "%s%sYou were killed by %s%s%s"
 msgstr "%s%sVous avez été tué par %s%s%s"
 
 #, c-format
-msgid "%sMode %s%s %s[%s%s%s]%s by %s%s"
-msgstr "%sMode %s%s %s[%s%s%s]%s par %s%s"
+msgid "%sMode %s%s %s[%s%s%s%s%s]%s by %s%s"
+msgstr "%sMode %s%s %s[%s%s%s%s%s]%s par %s%s"
 
 #, c-format
 msgid "%sUser mode %s[%s%s%s]%s by %s%s"

po/hu.po
@@ -20,7 +20,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2019-01-26 09:58+0100\n"
+"POT-Creation-Date: 2019-01-29 17:06+0100\n"
 "PO-Revision-Date: 2018-11-17 10:36+0100\n"
 "Last-Translator: Andras Voroskoi <voroskoi@frugalware.org>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -8537,7 +8537,7 @@ msgid "%s%sYou were killed by %s%s%s"
 msgstr "Az új neved: %s%s\n"
 
 #, fuzzy, c-format
-msgid "%sMode %s%s %s[%s%s%s]%s by %s%s"
+msgid "%sMode %s%s %s[%s%s%s%s%s]%s by %s%s"
 msgstr "A %s%s %s[%s%s%s]%s üzemmódot bekapcsolta: %s%s\n"
 
 #, fuzzy, c-format

po/it.po
@@ -20,8 +20,8 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2019-01-26 09:58+0100\n"
-"PO-Revision-Date: 2018-11-17 10:36+0100\n"
+"POT-Creation-Date: 2019-01-29 17:06+0100\n"
+"PO-Revision-Date: 2019-01-29 21:03+0100\n"
 "Last-Translator: Esteban I. Ruiz Moreno <exio4.com@gmail.com>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
 "Language: it\n"
@@ -9658,8 +9658,8 @@ msgid "%s%sYou were killed by %s%s%s"
 msgstr "%s%sSei stato ucciso da %s%s%s"
 
 #, c-format
-msgid "%sMode %s%s %s[%s%s%s]%s by %s%s"
-msgstr "%sModalità %s%s %s[%s%s%s]%s da %s%s"
+msgid "%sMode %s%s %s[%s%s%s%s%s]%s by %s%s"
+msgstr "%sModalità %s%s %s[%s%s%s%s%s]%s da %s%s"
 
 #, c-format
 msgid "%sUser mode %s[%s%s%s]%s by %s%s"

po/ja.po
@@ -20,7 +20,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2019-01-26 09:58+0100\n"
+"POT-Creation-Date: 2019-01-29 17:06+0100\n"
 "PO-Revision-Date: 2019-01-26 10:06+0100\n"
 "Last-Translator: AYANOKOUZI, Ryuunosuke <i38w7i3@yahoo.co.jp>\n"
 "Language-Team: Japanese <https://github.com/l/weechat/tree/master/"
@@ -10252,8 +10252,8 @@ msgstr "%s%s%s%s%s に kill されました %s(%s%s%s)"
 msgid "%s%sYou were killed by %s%s%s"
 msgstr "%s%s%s%s%s に kill されました"
 
-#, c-format
-msgid "%sMode %s%s %s[%s%s%s]%s by %s%s"
+#, fuzzy, c-format
+msgid "%sMode %s%s %s[%s%s%s%s%s]%s by %s%s"
 msgstr "%1$s%9$s%10$s%8$s がモードを変更 %2$s%3$s %4$s[%5$s%6$s%7$s]%8$s"
 
 #, c-format

commit 6e094991639cd17513ddd55e3f3f334a41ec2a07

irc: fix crash in parsing of IRC channel modes (issue #1296, closes #1297)
Regression was introduced by commit b38b2abe3b397af1133951fb5326cc5cbb80807e.

src/plugins/irc/irc-mode.c
@@ -320,11 +320,13 @@ int
 irc_mode_channel_set (struct t_irc_server *server,
                       struct t_irc_channel *channel,
                       const char *host,
-                      const char *modes)
+                      const char *modes,
+                      const char *modes_arguments)
 {
-    char *pos_args, *str_modes, set_flag, **argv, *pos, *ptr_arg, chanmode_type;
+    const char *pos, *ptr_arg;
+    char set_flag, **argv, chanmode_type;
     int argc, current_arg, update_channel_modes, channel_modes_updated;
-    int smart_filter;
+    int smart_filter, end_modes;
     struct t_irc_nick *ptr_nick;
     struct t_irc_modelist *ptr_modelist;
     struct t_irc_modelist_item *ptr_item;
@@ -335,23 +337,8 @@ irc_mode_channel_set (struct t_irc_server *server,
     channel_modes_updated = 0;
     argc = 0;
     argv = NULL;
-    pos_args = strchr (modes, ' ');
-    if (pos_args)
-    {
-        str_modes = weechat_strndup (modes, pos_args - modes);
-        if (!str_modes)
-            return 0;
-        pos_args++;
-        while (pos_args[0] == ' ')
-            pos_args++;
-        argv = weechat_string_split (pos_args, " ", 0, 0, &argc);
-    }
-    else
-    {
-        str_modes = strdup (modes);
-        if (!str_modes)
-            return 0;
-    }
+    if (modes_arguments)
+        argv = weechat_string_split (modes_arguments, " ", 0, 0, &argc);
 
     current_arg = 0;
 
@@ -359,119 +346,122 @@ irc_mode_channel_set (struct t_irc_server *server,
                     && weechat_config_string (irc_config_look_smart_filter_mode)
                     && weechat_config_string (irc_config_look_smart_filter_mode)[0]) ? 1 : 0;
 
-    if (str_modes && str_modes[0])
+    end_modes = 0;
+    set_flag = '+';
+    pos = modes;
+    while (pos[0])
     {
-        set_flag = '+';
-        pos = str_modes;
-        while (pos && pos[0])
+        switch (pos[0])
         {
-            switch (pos[0])
-            {
-                case ':':
-                case ' ':
-                    break;
-                case '+':
-                    set_flag = '+';
-                    break;
-                case '-':
-                    set_flag = '-';
-                    break;
-                default:
-                    update_channel_modes = 1;
-                    chanmode_type = irc_mode_get_chanmode_type (server, pos[0]);
-                    ptr_arg = NULL;
-                    switch (chanmode_type)
-                    {
-                        case 'A': /* always argument */
-                            update_channel_modes = 0;
-                            ptr_arg = (current_arg < argc) ?
-                                argv[current_arg] : NULL;
-                            break;
-                        case 'B': /* always argument */
-                            ptr_arg = (current_arg < argc) ?
-                                argv[current_arg] : NULL;
-                            break;
-                        case 'C': /* argument if set */
-                            ptr_arg = ((set_flag == '+') && (current_arg < argc)) ?
-                                argv[current_arg] : NULL;
-                            break;
-                        case 'D': /* no argument */
-                            break;
-                    }
-                    if (ptr_arg)
-                        current_arg++;
-
-                    if (smart_filter
-                        && !irc_mode_smart_filtered (server, pos[0]))
-                    {
-                        smart_filter = 0;
-                    }
-
-                    if (pos[0] == 'k')
-                    {
-                        /* channel key */
-                        if (set_flag == '-')
-                        {
-                            if (channel->key)
-                            {
-                                free (channel->key);
-                                channel->key = NULL;
-                            }
-                        }
-                        else if ((set_flag == '+')
-                                 && ptr_arg && (strcmp (ptr_arg, "*") != 0))
-                        {
-                            /* replace key for +k, but ignore "*" as new key */
-                            if (channel->key)
-                                free (channel->key);
-                            channel->key = strdup (ptr_arg);
-                        }
-                    }
-                    else if (pos[0] == 'l')
-                    {
-                        /* channel limit */
-                        if (set_flag == '-')
-                            channel->limit = 0;
-                        if ((set_flag == '+') && ptr_arg)
-                        {
-                            channel->limit = atoi (ptr_arg);
-                        }
-                    }
-                    else if ((chanmode_type != 'A')
-                             && (irc_server_get_prefix_mode_index (server,
-                                                                   pos[0]) >= 0))
-                    {
-                        /* mode for nick */
+            case ':':
+                break;
+            case ' ':
+                end_modes = 1;
+                break;
+            case '+':
+                set_flag = '+';
+                break;
+            case '-':
+                set_flag = '-';
+                break;
+            default:
+                update_channel_modes = 1;
+                chanmode_type = irc_mode_get_chanmode_type (server, pos[0]);
+                ptr_arg = NULL;
+                switch (chanmode_type)
+                {
+                    case 'A': /* always argument */
                         update_channel_modes = 0;
-                        if (ptr_arg)
+                        ptr_arg = (current_arg < argc) ?
+                            argv[current_arg] : NULL;
+                        break;
+                    case 'B': /* always argument */
+                        ptr_arg = (current_arg < argc) ?
+                            argv[current_arg] : NULL;
+                        break;
+                    case 'C': /* argument if set */
+                        ptr_arg = ((set_flag == '+') && (current_arg < argc)) ?
+                            argv[current_arg] : NULL;
+                        break;
+                    case 'D': /* no argument */
+                        break;
+                }
+                if (ptr_arg)
+                    current_arg++;
+
+                if (smart_filter
+                    && !irc_mode_smart_filtered (server, pos[0]))
+                {
+                    smart_filter = 0;
+                }
+
+                if (pos[0] == 'k')
+                {
+                    /* channel key */
+                    if (set_flag == '-')
+                    {
+                        if (channel->key)
                         {
-                            ptr_nick = irc_nick_search (server, channel,
-                                                        ptr_arg);
-                            if (ptr_nick)
+                            free (channel->key);
+                            channel->key = NULL;
+                        }
+                    }
+                    else if ((set_flag == '+')
+                             && ptr_arg && (strcmp (ptr_arg, "*") != 0))
+                    {
+                        /* replace key for +k, but ignore "*" as new key */
+                        if (channel->key)
+                            free (channel->key);
+                        channel->key = strdup (ptr_arg);
+                    }
+                }
+                else if (pos[0] == 'l')
+                {
+                    /* channel limit */
+                    if (set_flag == '-')
+                        channel->limit = 0;
+                    if ((set_flag == '+') && ptr_arg)
+                    {
+                        channel->limit = atoi (ptr_arg);
+                    }
+                }
+                else if ((chanmode_type != 'A')
+                         && (irc_server_get_prefix_mode_index (server,
+                                                               pos[0]) >= 0))
+                {
+                    /* mode for nick */
+                    update_channel_modes = 0;
+                    if (ptr_arg)
+                    {
+                        ptr_nick = irc_nick_search (server, channel,
+                                                    ptr_arg);
+                        if (ptr_nick)
+                        {
+                            irc_nick_set_mode (server, channel, ptr_nick,
+                                               (set_flag == '+'), pos[0]);
+                            /*
+                             * disable smart filtering if mode is sent
+                             * to me, or based on the nick speaking time
+                             */
+                            if (smart_filter
+                                && ((irc_server_strcasecmp (server,
+                                                            ptr_nick->name,
+                                                            server->nick) == 0)
+                                    || irc_channel_nick_speaking_time_search (server,
+                                                                              channel,
+                                                                              ptr_nick->name,
+                                                                              1)))
                             {
-                                irc_nick_set_mode (server, channel, ptr_nick,
-                                                   (set_flag == '+'), pos[0]);
-                                /*
-                                 * disable smart filtering if mode is sent
-                                 * to me, or based on the nick speaking time
-                                 */
-                                if (smart_filter
-                                    && ((irc_server_strcasecmp (server,
-                                                                ptr_nick->name,
-                                                                server->nick) == 0)
-                                        || irc_channel_nick_speaking_time_search (server,
-                                                                                  channel,
-                                                                                  ptr_nick->name,
-                                                                                  1)))
-                                {
-                                    smart_filter = 0;
-                                }
+                                smart_filter = 0;
                             }
                         }
                     }
-                    else if (chanmode_type == 'A')
+                }
+                else if (chanmode_type == 'A')
+                {
+                    /* modelist modes */
+                    if (ptr_arg)
                     {
-                        /* modelist modes */
                         ptr_modelist = irc_modelist_search (channel, pos[0]);
                         if (ptr_modelist)
                         {
@@ -489,21 +479,21 @@ irc_mode_channel_set (struct t_irc_server *server,
                             }
                         }
                     }
+                }
 
-                    if (update_channel_modes)
-                    {
-                        irc_mode_channel_update (server, channel, set_flag,
-                                                 pos[0], ptr_arg);
-                        channel_modes_updated = 1;
-                    }
-                    break;
-            }
-            pos++;
+                if (update_channel_modes)
+                {
+                    irc_mode_channel_update (server, channel, set_flag,
+                                             pos[0], ptr_arg);
+                    channel_modes_updated = 1;
+                }
+                break;
         }
+        if (end_modes)
+            break;
+        pos++;
     }
 
-    if (str_modes)
-        free (str_modes);
     if (argv)
         weechat_string_free_split (argv);
 

src/plugins/irc/irc-mode.h
@@ -28,7 +28,8 @@ extern char irc_mode_get_chanmode_type (struct t_irc_server *server,
 extern int irc_mode_channel_set (struct t_irc_server *server,
                                  struct t_irc_channel *channel,
                                  const char *host,
-                                 const char *modes);
+                                 const char *modes,
+                                 const char *modes_arguments);
 extern void irc_mode_user_set (struct t_irc_server *server, const char *modes,
                                int reset_modes);
 

src/plugins/irc/irc-modelist.c
@@ -257,6 +257,9 @@ irc_modelist_item_new (struct t_irc_modelist *modelist,
 {
     struct t_irc_modelist_item *new_item;
 
+    if (!mask)
+        return NULL;
+
     /* alloc memory for new item */
     if ((new_item = malloc (sizeof (*new_item))) == NULL)
     {

src/plugins/irc/irc-protocol.c
@@ -1398,9 +1398,8 @@ IRC_PROTOCOL_CALLBACK(mode)
     IRC_PROTOCOL_CHECK_HOST;
 
     pos_modes = (argv[3][0] == ':') ? argv[3] + 1 : argv[3];
-    pos_modes_args = NULL;
-    if (argc > 4)
-        pos_modes_args = (argv_eol[4][0] == ':') ? argv_eol[4] + 1 : argv_eol[4];
+    pos_modes_args = (argc > 4) ?
+        ((argv_eol[4][0] == ':') ? argv_eol[4] + 1 : argv_eol[4]) : NULL;
 
     if (irc_channel_is_channel (server, argv[2]))
     {
@@ -1409,7 +1408,7 @@ IRC_PROTOCOL_CALLBACK(mode)
         if (ptr_channel)
         {
             smart_filter = irc_mode_channel_set (server, ptr_channel, host,
-                                                 pos_modes);
+                                                 pos_modes, pos_modes_args);
         }
         local_mode = (irc_server_strcasecmp (server, nick, server->nick) == 0);
         ptr_nick = irc_nick_search (server, ptr_channel, nick);
@@ -3546,7 +3545,7 @@ IRC_PROTOCOL_CALLBACK(324)
         if (argc > 4)
         {
             (void) irc_mode_channel_set (server, ptr_channel, host,
-                                         ptr_channel->modes);
+                                         ptr_modes, NULL);
         }
     }
     if (!ptr_channel