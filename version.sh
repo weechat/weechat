4c9b61755bb2e56cc3aa02e04bf8751bcc0b4901
Sébastien Helleu <flashcode@flashtux.org>
2019-06-04 23:12:40 +0200

    Version 2.5-rc2

--- version.sh
+++ version.sh
@@ -34,7 +34,7 @@
 
 WEECHAT_STABLE=2.4
 WEECHAT_DEVEL=2.5
-WEECHAT_DEVEL_FULL=2.5-rc1
+WEECHAT_DEVEL_FULL=2.5-rc2
 
 if [ $# -lt 1 ]; then
     echo >&2 "Syntax: $0 stable|devel|devel-full|devel-major|devel-minor|devel-patch"
