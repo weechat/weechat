35903021836f7aa3cc0e800b3d719c2bd06b384c
Sébastien Helleu <flashcode@flashtux.org>
2020-03-08 13:56:30 +0100

    irc: copy temporary server flag in command /server copy

--- src/plugins/irc/irc-server.c
+++ src/plugins/irc/irc-server.c
@@ -2024,40 +2024,43 @@ irc_server_copy (struct t_irc_server *server, const char *new_name)
         return NULL;
 
     new_server = irc_server_alloc (new_name);
-    if (new_server)
+    if (!new_server)
+        return NULL;
+
+    /* duplicate temporary server flag */
+    new_server->temp_server = server->temp_server;
+
+    /* duplicate options */
+    length = 32 + strlen (server->name) + 1;
+    mask = malloc (length);
+    if (!mask)
+        return 0;
+    snprintf (mask, length, "irc.server.%s.*", server->name);
+    infolist = weechat_infolist_get ("option", NULL, mask);
+    free (mask);
+    if (infolist)
     {
-        /* duplicate options */
-        length = 32 + strlen (server->name) + 1;
-        mask = malloc (length);
-        if (!mask)
-            return 0;
-        snprintf (mask, length, "irc.server.%s.*", server->name);
-        infolist = weechat_infolist_get ("option", NULL, mask);
-        free (mask);
-        if (infolist)
+        while (weechat_infolist_next (infolist))
         {
-            while (weechat_infolist_next (infolist))
+            if (!weechat_infolist_integer (infolist, "value_is_null"))
             {
-                if (!weechat_infolist_integer (infolist, "value_is_null"))
+                option_name = weechat_infolist_string (infolist,
+                                                       "option_name");
+                pos = strrchr (option_name, '.');
+                if (pos)
                 {
-                    option_name = weechat_infolist_string (infolist,
-                                                           "option_name");
-                    pos = strrchr (option_name, '.');
-                    if (pos)
+                    index_option = irc_server_search_option (pos + 1);
+                    if (index_option >= 0)
                     {
-                        index_option = irc_server_search_option (pos + 1);
-                        if (index_option >= 0)
-                        {
-                            weechat_config_option_set (
-                                new_server->options[index_option],
-                                weechat_infolist_string (infolist, "value"),
-                                1);
-                        }
+                        weechat_config_option_set (
+                            new_server->options[index_option],
+                            weechat_infolist_string (infolist, "value"),
+                            1);
                     }
                 }
             }
-            weechat_infolist_free (infolist);
         }
+        weechat_infolist_free (infolist);
     }
 
     return new_server;
