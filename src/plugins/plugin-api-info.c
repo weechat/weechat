aa86d82796bad746c65693435b5472a44b1beb13
Sébastien Helleu <flashcode@flashtux.org>
2020-05-16 14:23:18 +0200

    core: rename info_hashtable "gui_focus_info" to "focus_info" (issue #1257)

--- src/plugins/plugin-api-info.c
+++ src/plugins/plugin-api-info.c
@@ -1862,11 +1862,11 @@ plugin_api_info_init ()
 
     /* WeeChat core info_hashtable hooks */
     hook_info_hashtable (NULL,
-        "gui_focus_info",
+        "focus_info",
         N_("get focus info"),
         /* TRANSLATORS: please do not translate key names (enclosed by quotes) */
         N_("\"x\": x coordinate, \"y\": y coordinate"),
-        N_("see hook_focus"),
+        N_("see hook_focus in Plugin API reference"),
         &gui_focus_info_hashtable_gui_focus_info_cb, NULL, NULL);
 
     /* WeeChat core infolist hooks */
