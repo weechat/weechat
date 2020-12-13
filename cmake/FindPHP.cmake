2b12b4077a4c463e27d7e0a3d1d11350fc85f82c
Sébastien Helleu <flashcode@flashtux.org>
2020-12-13 11:52:29 +0100

    php: add detection of PHP 8.0, fix compilation errors

--- cmake/FindPHP.cmake
+++ cmake/FindPHP.cmake
@@ -24,11 +24,12 @@ endif()
 
 find_package(PkgConfig)
 if(PKG_CONFIG_FOUND)
-  pkg_search_module(PHP php7)
+  pkg_search_module(PHP php8 php7)
 endif()
 
 if(NOT PHP_FOUND)
   find_program(PHP_CONFIG_EXECUTABLE NAMES
+    php-config8.0 php-config80
     php-config7.4 php-config74
     php-config7.3 php-config73
     php-config7.2 php-config72
@@ -41,9 +42,9 @@ if(NOT PHP_FOUND)
     execute_process(COMMAND ${PHP_CONFIG_EXECUTABLE} --includes OUTPUT_VARIABLE PHP_INCLUDE_DIRS OUTPUT_STRIP_TRAILING_WHITESPACE)
     execute_process(COMMAND ${PHP_CONFIG_EXECUTABLE} --libs OUTPUT_VARIABLE PHP_LIBS OUTPUT_STRIP_TRAILING_WHITESPACE)
     execute_process(COMMAND ${PHP_CONFIG_EXECUTABLE} --version OUTPUT_VARIABLE PHP_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
-    if(${PHP_VERSION} MATCHES "^7")
+    if(${PHP_VERSION} MATCHES "^[78]")
       find_library(PHP_LIB
-        NAMES php7.4 php7.3 php7.2 php7.1 php7.0 php7
+        NAMES php8.0 php7.4 php7.3 php7.2 php7.1 php7.0 php7
         HINTS ${PHP_LIB_PREFIX} ${PHP_LIB_PREFIX}/lib ${PHP_LIB_PREFIX}/lib64
       )
       if(PHP_LIB)
