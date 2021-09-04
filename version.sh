3e180a3c9082cf214efa72935184861b8f8a8540
Sébastien Helleu <flashcode@flashtux.org>
2021-09-04 13:16:44 +0200

    Version 3.2.1

--- version.sh
+++ version.sh
@@ -32,9 +32,9 @@
 #     devel-patch  the patch version of devel (e.g. 2 for version 1.4.2)
 #
 
-WEECHAT_STABLE=3.2
+WEECHAT_STABLE=3.2.1
 WEECHAT_DEVEL=3.2.1
-WEECHAT_DEVEL_FULL=3.2.1-dev
+WEECHAT_DEVEL_FULL=3.2.1
 
 if [ $# -lt 1 ]; then
     echo >&2 "Syntax: $0 stable|devel|devel-full|devel-major|devel-minor|devel-patch"
