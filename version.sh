2e1d16b7e5240226cca70adc6143b7b938f934e6
Sébastien Helleu <flashcode@flashtux.org>
2022-03-13 20:19:39 +0100

    Version 3.4.1-dev

--- version.sh
+++ version.sh
@@ -33,8 +33,8 @@
 #
 
 WEECHAT_STABLE=3.4
-WEECHAT_DEVEL=3.4
-WEECHAT_DEVEL_FULL=3.4
+WEECHAT_DEVEL=3.4.1
+WEECHAT_DEVEL_FULL=3.4.1-dev
 
 if [ $# -lt 1 ]; then
     echo >&2 "Syntax: $0 stable|devel|devel-full|devel-major|devel-minor|devel-patch"
