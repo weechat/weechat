weechat/weechat

commit 66571a0b634ef28307f6d6ceef1ba29c091ca524

core: add configuration version, add API function config_set_version

src/core/wee-config-file.c
@@ -37,6 +37,7 @@
 #include "wee-config-file.h"
 #include "wee-arraylist.h"
 #include "wee-config.h"
+#include "wee-hashtable.h"
 #include "wee-hdata.h"
 #include "wee-hook.h"
 #include "wee-infolist.h"
@@ -239,6 +240,10 @@ config_file_new (struct t_weechat_plugin *plugin, const char *name,
             return NULL;
         }
         new_config_file->file = NULL;
+        new_config_file->version = 1;
+        new_config_file->callback_update = NULL;
+        new_config_file->callback_update_pointer = NULL;
+        new_config_file->callback_update_data = NULL;
         new_config_file->callback_reload = callback_reload;
         new_config_file->callback_reload_pointer = callback_reload_pointer;
         new_config_file->callback_reload_data = callback_reload_data;
@@ -249,6 +254,38 @@ config_file_new (struct t_weechat_plugin *plugin, const char *name,
     }
 
     return new_config_file;
+}
+
+/*
+ * Sets configuration file version and a callback to update config
+ * sections/options on-the-fly when the config is read.
+ *
+ * Returns:
+ *   1: OK
+ *   0: error
+ */
+
+int
+config_file_set_version (struct t_config_file *config_file,
+                         int version,
+                         struct t_hashtable *(*callback_update)(const void *pointer,
+                                                                void *data,
+                                                                struct t_config_file *config_file,
+                                                                int version_read,
+                                                                struct t_hashtable *data_read),
+                         const void *callback_update_pointer,
+                         void *callback_update_data)
+{
+    if (version < 1)
+        return 0;
+
+    config_file->version = version;
+
+    config_file->callback_update = callback_update;
+    config_file->callback_update_pointer = callback_update_pointer;
+    config_file->callback_update_data = callback_update_data;
+
+    return 1;
 }
 
 /*
@@ -2693,6 +2730,17 @@ config_file_write_internal (struct t_config_file *config_file,
         goto error;
     }
 
+    /* write config version (if different from 1) */
+    if (config_file->version > 1)
+    {
+        if (!string_fprintf (config_file->file,
+                             "\nconfig_version = %d\n",
+                             config_file->version))
+        {
+            goto error;
+        }
+    }
+
     /* write all sections */
     for (ptr_section = config_file->sections; ptr_section;
          ptr_section = ptr_section->next_section)
@@ -2797,6 +2845,142 @@ config_file_write (struct t_config_file *config_file)
 }
 
 /*
+ * Parses configuration version.
+ *
+ * Returns:
+ *   >= 1: configuration version
+ *     -1: error
+ */
+
+int
+config_file_parse_version (const char *version)
+{
+    long number;
+    char *error;
+
+    number = strtoll (version, &error, 10);
+    if (!error || error[0])
+        return -1;
+
+    return (number < 1) ? -1 : (int)number;
+}
+
+/*
+ * Updates data read from config file: either section or option + value.
+ * The update callback (if defined in config) is called if the config version
+ * read in file is less than to the current config version.
+ *
+ * Parameters "section", "option" and "value" are updated in place: if the
+ * callback gives a new value, they are first freed and allocated again with
+ * the new value (or set to NULL for the value if the callback returns
+ * special key "value_null").
+ *
+ * Section can be updated only if option and value are NULL (ie if we are
+ * reading a section line like "[section]").
+ */
+
+void
+config_file_update_data_read (struct t_config_file *config_file,
+                              const char *section,
+                              const char *option,
+                              const char *value,
+                              char **ret_section,
+                              char **ret_option,
+                              char **ret_value)
+{
+    struct t_hashtable *data_read, *hashtable;
+    const char *ptr_section, *ptr_option, *ptr_value;
+    int value_null;
+
+    /* do nothing if config is already the latest version */
+    if (config_file->version_read >= config_file->version)
+        return;
+
+    /* do nothing if there's no update callback */
+    if (!config_file->callback_update)
+        return;
+
+    value_null = 0;
+
+    data_read = hashtable_new (
+        32,
+        WEECHAT_HASHTABLE_STRING,
+        WEECHAT_HASHTABLE_STRING,
+        NULL, NULL);
+    if (!data_read)
+        return;
+
+    hashtable_set (data_read, "config", config_file->name);
+    if (section)
+        hashtable_set (data_read, "section", section);
+    if (option)
+    {
+        hashtable_set (data_read, "option", option);
+        if (value)
+        {
+            hashtable_set (data_read, "value", value);
+        }
+        else
+        {
+            hashtable_set (data_read, "value_null", "1");
+            value_null = 1;
+        }
+    }
+
+    hashtable = (config_file->callback_update)
+        (config_file->callback_update_pointer,
+         config_file->callback_update_data,
+         config_file,
+         config_file->version_read,
+         data_read);
+
+    if (hashtable)
+    {
+        /* if reading a section line, we can update its name */
+        if (section && !option && ret_section)
+        {
+            ptr_section = hashtable_get (hashtable, "section");
+            if (ptr_section && ptr_section[0])
+            {
+                if (*ret_section)
+                    free (*ret_section);
+                *ret_section = strdup (ptr_section);
+            }
+        }
+
+        /* if reading an option line, we can update its name and value */
+        if (section && option)
+        {
+            /* option name */
+            if (ret_option)
+            {
+                ptr_option = hashtable_get (hashtable, "option");
+                if (ptr_option)
+                {
+                    if (*ret_option)
+                        free (*ret_option);
+                    *ret_option = strdup (ptr_option);
+                }
+            }
+            /* value */
+            if (ret_value)
+            {
+                ptr_value = hashtable_get (hashtable, "value");
+                if (!value_null && hashtable_has_key (hashtable, "value_null"))
+                    ptr_value = NULL;
+                if (*ret_value)
+                    free (*ret_value);
+                *ret_value = (ptr_value) ? strdup (ptr_value) : NULL;
+            }
+        }
+    }
+
+    if (hashtable && (hashtable != data_read))
+        hashtable_free (hashtable);
+    hashtable_free (data_read);
+}
+
+/*
  * Reads a configuration file (this function must not be called directly).
  *
  * Returns:
@@ -2808,7 +2992,7 @@ config_file_write (struct t_config_file *config_file)
 int
 config_file_read_internal (struct t_config_file *config_file, int reload)
 {
-    int filename_length, line_number, rc, length;
+    int filename_length, line_number, rc, length, version;
     char *filename, *section, *option, *value;
     struct t_config_section *ptr_section;
     struct t_config_option *ptr_option;
@@ -2816,6 +3000,8 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
 
     if (!config_file)
         return WEECHAT_CONFIG_READ_FILE_NOT_FOUND;
+
+    config_file->version_read = 1;
 
     /* build filename */
     filename_length = strlen (weechat_config_dir) + strlen (DIR_SEPARATOR) +
@@ -2911,6 +3097,9 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
                 section = string_strndup (ptr_line + 1, pos - ptr_line - 1);
                 if (section)
                 {
+                    config_file_update_data_read (config_file,
+                                                  section, NULL, NULL,
+                                                  &section, NULL, NULL);
                     ptr_section = config_file_search_section (config_file,
                                                               section);
                     if (!ptr_section)
@@ -2983,7 +3172,46 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
             option = strdup (ptr_line);
         }
 
-        if (ptr_section && ptr_section->callback_read)
+        if (!ptr_section && (strcmp (option, CONFIG_VERSION_OPTION) == 0))
+        {
+            version = config_file_parse_version (pos);
+            if (version < 0)
+            {
+                gui_chat_printf (
+                    NULL,
+                    _("%sWarning: %s, line %d: invalid config "
+                      "version: %s"),
+                    gui_chat_prefix[GUI_CHAT_PREFIX_ERROR],
+                    filename, line_number,
+                    line);
+            }
+            else
+            {
+                config_file->version_read = version;
+            }
+            continue;
+        }
+
+        if (!ptr_section)
+        {
+            gui_chat_printf (NULL,
+                             _("%sWarning: %s, line %d: "
+                               "option outside section: %s"),
+                             gui_chat_prefix[GUI_CHAT_PREFIX_ERROR],
+                             filename, line_number,
+                             line);
+            continue;
+        }
+
+        config_file_update_data_read (config_file,
+                                      ptr_section->name, option, value,
+                                      NULL, &option, &value);
+
+        /* option has been ignored by the update callback? */
+        if (!option || !option[0])
+            continue;
+
+        if (ptr_section->callback_read)
         {
             ptr_option = NULL;
             rc = (ptr_section->callback_read)
@@ -3007,8 +3235,7 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
             }
             else
             {
-                if (ptr_section
-                    && ptr_section->callback_create_option)
+                if (ptr_section->callback_create_option)
                 {
                     rc = (int) (ptr_section->callback_create_option) (
                         ptr_section->callback_create_option_pointer,
@@ -3024,25 +3251,13 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
         switch (rc)
         {
             case WEECHAT_CONFIG_OPTION_SET_OPTION_NOT_FOUND:
-                if (ptr_section)
-                {
-                    gui_chat_printf (NULL,
-                                     _("%sWarning: %s, line %d: "
-                                       "unknown option for section \"%s\": %s"),
-                                     gui_chat_prefix[GUI_CHAT_PREFIX_ERROR],
-                                     filename, line_number,
-                                     ptr_section->name,
-                                     line);
-                }
-                else
-                {
-                    gui_chat_printf (NULL,
-                                     _("%sWarning: %s, line %d: "
-                                       "option outside section: %s"),
-                                     gui_chat_prefix[GUI_CHAT_PREFIX_ERROR],
-                                     filename, line_number,
-                                     line);
-                }
+                gui_chat_printf (NULL,
+                                 _("%sWarning: %s, line %d: "
+                                   "unknown option for section \"%s\": %s"),
+                                 gui_chat_prefix[GUI_CHAT_PREFIX_ERROR],
+                                 filename, line_number,
+                                 ptr_section->name,
+                                 line);
                 break;
             case WEECHAT_CONFIG_OPTION_SET_ERROR:
                 gui_chat_printf (NULL,
@@ -3315,6 +3530,8 @@ config_file_free (struct t_config_file *config_file)
         (config_file->next_config)->prev_config = config_file->prev_config;
 
     /* free data */
+    if (config_file->callback_update_data)
+        free (config_file->callback_update_data);
     if (config_file->callback_reload_data)
         free (config_file->callback_reload_data);
 
@@ -3380,6 +3597,7 @@ config_file_hdata_config_file_cb (const void *pointer, void *data,
         HDATA_VAR(struct t_config_file, name, STRING, 0, NULL, NULL);
         HDATA_VAR(struct t_config_file, filename, STRING, 0, NULL, NULL);
         HDATA_VAR(struct t_config_file, file, POINTER, 0, NULL, NULL);
+        HDATA_VAR(struct t_config_file, version, INTEGER, 0, NULL, NULL);
         HDATA_VAR(struct t_config_file, callback_reload, POINTER, 0, NULL, NULL);
         HDATA_VAR(struct t_config_file, callback_reload_pointer, POINTER, 0, NULL, NULL);
         HDATA_VAR(struct t_config_file, callback_reload_data, POINTER, 0, NULL, NULL);

src/core/wee-config-file.h
@@ -23,6 +23,8 @@
 
 #include <stdio.h>
 
+#define CONFIG_VERSION_OPTION  "config_version"
+
 #define CONFIG_PRIORITY_DEFAULT 1000
 
 #define CONFIG_BOOLEAN(option) (*((int *)((option)->value)))
@@ -54,7 +56,17 @@ struct t_config_file
     char *filename;                        /* filename (without path)       */
                                            /* (example: "weechat.conf")     */
     FILE *file;                            /* file pointer                  */
-    int (*callback_reload)                 /* callback for reloading file   */
+    int version;                           /* config version (default=1)    */
+    int version_read;                      /* config version read in file   */
+    struct t_hashtable *(*callback_update) /* callback for version update   */
+    (const void *pointer,
+     void *data,
+     struct t_config_file *config_file,
+     int version_read,
+     struct t_hashtable *data_read);
+    const void *callback_update_pointer;   /* pointer sent to callback      */
+    void *callback_update_data;            /* data sent to callback         */
+    int (*callback_reload)                 /* callback for config reload    */
     (const void *pointer,
      void *data,
      struct t_config_file *config_file);
@@ -178,6 +190,15 @@ extern struct t_config_file *config_file_new (struct t_weechat_plugin *plugin,
                                                                      struct t_config_file *config_file),
                                               const void *callback_reload_pointer,
                                               void *callback_reload_data);
+extern int config_file_set_version (struct t_config_file *config_file,
+                                    int version,
+                                    struct t_hashtable *(*callback_update)(const void *pointer,
+                                                                           void *data,
+                                                                           struct t_config_file *config_file,
+                                                                           int version_read,
+                                                                           struct t_hashtable *data_read),
+                                    const void *callback_update_pointer,
+                                    void *callback_update_data);
 extern struct t_arraylist *config_file_get_configs_by_priority ();
 extern struct t_config_section *config_file_new_section (struct t_config_file *config_file,
                                                          const char *name,

src/plugins/guile/weechat-guile-api.c
@@ -902,6 +902,62 @@ weechat_guile_api_config_new (SCM name, SCM function, SCM data)
     API_RETURN_STRING(result);
 }
 
+struct t_hashtable *
+weechat_guile_api_config_update_cb (const void *pointer, void *data,
+                                    struct t_config_file *config_file,
+                                    int version_read,
+                                    struct t_hashtable *data_read)
+{
+    struct t_plugin_script *script;
+    void *func_argv[4];
+    char empty_arg[1] = { '\0' };
+    const char *ptr_function, *ptr_data;
+    struct t_hashtable *ret_hashtable;
+
+    script = (struct t_plugin_script *)pointer;
+    plugin_script_get_function_and_data (data, &ptr_function, &ptr_data);
+
+    if (ptr_function && ptr_function[0])
+    {
+        func_argv[0] = (ptr_data) ? (char *)ptr_data : empty_arg;
+        func_argv[1] = (char *)API_PTR2STR(config_file);
+        func_argv[2] = &version_read;
+        func_argv[3] = data_read;
+
+        ret_hashtable = weechat_guile_exec (script,
+                                            WEECHAT_SCRIPT_EXEC_HASHTABLE,
+                                            ptr_function,
+                                            "ssih", func_argv);
+
+        return ret_hashtable;
+    }
+
+    return NULL;
+}
+
+SCM
+weechat_guile_api_config_set_version (SCM config_file, SCM version,
+                                      SCM function, SCM data)
+{
+    int rc;
+
+    API_INIT_FUNC(1, "config_set_version", API_RETURN_INT(0));
+    if (!scm_is_string (config_file) || !scm_is_integer (version)
+        || !scm_is_string (function) || !scm_is_string (data))
+        API_WRONG_ARGS(API_RETURN_INT(0));
+
+    rc = plugin_script_api_config_set_version (
+        weechat_guile_plugin,
+        guile_current_script,
+        API_STR2PTR(API_SCM_TO_STRING(config_file)),
+        scm_to_int (version),
+        &weechat_guile_api_config_update_cb,
+        API_SCM_TO_STRING(function),
+        API_SCM_TO_STRING(data));
+
+    API_RETURN_INT(rc);
+}
+
 int
 weechat_guile_api_config_read_cb (const void *pointer, void *data,
                                   struct t_config_file *config_file,
@@ -5150,6 +5206,7 @@ weechat_guile_api_module_init (void *data)
     API_DEF_FUNC(list_remove_all, 1);
     API_DEF_FUNC(list_free, 1);
     API_DEF_FUNC(config_new, 3);
+    API_DEF_FUNC(config_set_version, 4);
     API_DEF_FUNC(config_new_section, 1);
     API_DEF_FUNC(config_search_section, 2);
     API_DEF_FUNC(config_new_option, 1);

src/plugins/javascript/weechat-js-api.cpp
@@ -835,6 +835,63 @@ API_FUNC(config_new)
     API_RETURN_STRING(result);
 }
 
+struct t_hashtable *
+weechat_js_api_config_update_cb (const void *pointer, void *data,
+                                 struct t_config_file *config_file,
+                                 int version_read,
+                                 struct t_hashtable *data_read)
+{
+    struct t_plugin_script *script;
+    void *func_argv[4];
+    char empty_arg[1] = { '\0' };
+    const char *ptr_function, *ptr_data;
+    struct t_hashtable *ret_hashtable;
+
+    script = (struct t_plugin_script *)pointer;
+    plugin_script_get_function_and_data (data, &ptr_function, &ptr_data);
+
+    if (ptr_function && ptr_function[0])
+    {
+        func_argv[0] = (ptr_data) ? (char *)ptr_data : empty_arg;
+        func_argv[1] = (char *)API_PTR2STR(config_file);
+        func_argv[2] = &version_read;
+        func_argv[3] = data_read;
+
+        ret_hashtable = (struct t_hashtable *)weechat_js_exec (
+            script,
+            WEECHAT_SCRIPT_EXEC_HASHTABLE,
+            ptr_function,
+            "ssih", func_argv);
+
+        return ret_hashtable;
+    }
+
+    return NULL;
+}
+
+API_FUNC(config_set_version)
+{
+    int rc, version;
+
+    API_INIT_FUNC(1, "config_set_version", "siss", API_RETURN_INT(0));
+
+    v8::String::Utf8Value config_file(args[0]);
+    version = args[1]->IntegerValue();
+    v8::String::Utf8Value function(args[2]);
+    v8::String::Utf8Value data(args[3]);
+
+    rc = plugin_script_api_config_set_version (
+        weechat_js_plugin,
+        js_current_script,
+        (struct t_config_file *)API_STR2PTR(*config_file),
+        version,
+        &weechat_js_api_config_update_cb,
+        *function,
+        *data);
+
+    API_RETURN_INT(rc);
+}
+
 int
 weechat_js_api_config_read_cb (const void *pointer, void *data,
                                struct t_config_file *config_file,
@@ -5092,6 +5149,7 @@ WeechatJsV8::loadLibs()
     API_DEF_FUNC(list_remove_all);
     API_DEF_FUNC(list_free);
     API_DEF_FUNC(config_new);
+    API_DEF_FUNC(config_set_version);
     API_DEF_FUNC(config_new_section);
     API_DEF_FUNC(config_search_section);
     API_DEF_FUNC(config_new_option);

src/plugins/lua/weechat-lua-api.c
@@ -940,6 +940,65 @@ API_FUNC(config_new)
     API_RETURN_STRING(result);
 }
 
+struct t_hashtable *
+weechat_lua_api_config_update_cb (const void *pointer, void *data,
+                                  struct t_config_file *config_file,
+                                  int version_read,
+                                  struct t_hashtable *data_read)
+{
+    struct t_plugin_script *script;
+    void *func_argv[4];
+    char empty_arg[1] = { '\0' };
+    const char *ptr_function, *ptr_data;
+    struct t_hashtable *ret_hashtable;
+
+    script = (struct t_plugin_script *)pointer;
+    plugin_script_get_function_and_data (data, &ptr_function, &ptr_data);
+
+    if (ptr_function && ptr_function[0])
+    {
+        func_argv[0] = (ptr_data) ? (char *)ptr_data : empty_arg;
+        func_argv[1] = (char *)API_PTR2STR(config_file);
+        func_argv[2] = &version_read;
+        func_argv[3] = data_read;
+
+        ret_hashtable = weechat_lua_exec (script,
+                                          WEECHAT_SCRIPT_EXEC_HASHTABLE,
+                                          ptr_function,
+                                          "ssih", func_argv);
+
+        return ret_hashtable;
+    }
+
+    return NULL;
+}
+
+API_FUNC(config_set_version)
+{
+    const char *config_file, *function, *data;
+    int rc, version;
+
+    API_INIT_FUNC(1, "config_set_version", API_RETURN_INT(0));
+    if (lua_gettop (L) < 4)
+        API_WRONG_ARGS(API_RETURN_INT(0));
+
+    config_file = lua_tostring (L, -4);
+    version = lua_tonumber (L, -3);
+    function = lua_tostring (L, -2);
+    data = lua_tostring (L, -1);
+
+    rc = plugin_script_api_config_set_version (
+        weechat_lua_plugin,
+        lua_current_script,
+        API_STR2PTR(config_file),
+        version,
+        &weechat_lua_api_config_update_cb,
+        function,
+        data);
+
+    API_RETURN_INT(rc);
+}
+
 int
 weechat_lua_api_config_read_cb (const void *pointer, void *data,
                                 struct t_config_file *config_file,
@@ -5451,6 +5510,7 @@ const struct luaL_Reg weechat_lua_api_funcs[] = {
     API_DEF_FUNC(list_remove_all),
     API_DEF_FUNC(list_free),
     API_DEF_FUNC(config_new),
+    API_DEF_FUNC(config_set_version),
     API_DEF_FUNC(config_new_section),
     API_DEF_FUNC(config_search_section),
     API_DEF_FUNC(config_new_option),

src/plugins/perl/weechat-perl-api.c
@@ -881,6 +881,65 @@ API_FUNC(config_new)
     API_RETURN_STRING(result);
 }
 
+struct t_hashtable *
+weechat_perl_api_config_update_cb (const void *pointer, void *data,
+                                   struct t_config_file *config_file,
+                                   int version_read,
+                                   struct t_hashtable *data_read)
+{
+    struct t_plugin_script *script;
+    void *func_argv[4];
+    char empty_arg[1] = { '\0' };
+    const char *ptr_function, *ptr_data;
+    struct t_hashtable *ret_hashtable;
+
+    script = (struct t_plugin_script *)pointer;
+    plugin_script_get_function_and_data (data, &ptr_function, &ptr_data);
+
+    if (ptr_function && ptr_function[0])
+    {
+        func_argv[0] = (ptr_data) ? (char *)ptr_data : empty_arg;
+        func_argv[1] = (char *)API_PTR2STR(config_file);
+        func_argv[2] = &version_read;
+        func_argv[3] = data_read;
+
+        ret_hashtable = weechat_perl_exec (script,
+                                           WEECHAT_SCRIPT_EXEC_HASHTABLE,
+                                           ptr_function,
+                                           "ssih", func_argv);
+
+        return ret_hashtable;
+    }
+
+    return NULL;
+}
+
+API_FUNC(config_set_version)
+{
+    char *config_file, *function, *data;
+    int rc;
+    dXSARGS;
+
+    API_INIT_FUNC(1, "config_set_version", API_RETURN_INT(0));
+    if (items < 4)
+        API_WRONG_ARGS(API_RETURN_INT(0));
+
+    config_file = SvPV_nolen (ST (0));
+    function = SvPV_nolen (ST (2));
+    data = SvPV_nolen (ST (3));
+
+    rc = plugin_script_api_config_set_version (
+        weechat_perl_plugin,
+        perl_current_script,
+        API_STR2PTR(config_file),
+        SvIV (ST (1)), /* version */
+        &weechat_perl_api_config_update_cb,
+        function,
+        data);
+
+    API_RETURN_INT(rc);
+}
+
 int
 weechat_perl_api_config_section_read_cb (const void *pointer, void *data,
                                          struct t_config_file *config_file,
@@ -1088,7 +1147,7 @@ weechat_perl_api_config_section_delete_option_cb (const void *pointer, void *dat
 
 API_FUNC(config_new_section)
 {
-    char *cfg_file, *name, *function_read, *data_read;
+    char *config_file, *name, *function_read, *data_read;
     char *function_write, *data_write, *function_write_default;
     char *data_write_default, *function_create_option, *data_create_option;
     char *function_delete_option, *data_delete_option;
@@ -1100,7 +1159,7 @@ API_FUNC(config_new_section)
     if (items < 14)
         API_WRONG_ARGS(API_RETURN_EMPTY);
 
-    cfg_file = SvPV_nolen (ST (0));
+    config_file = SvPV_nolen (ST (0));
     name = SvPV_nolen (ST (1));
     function_read = SvPV_nolen (ST (4));
     data_read = SvPV_nolen (ST (5));
@@ -1117,7 +1176,7 @@ API_FUNC(config_new_section)
         plugin_script_api_config_new_section (
             weechat_perl_plugin,
             perl_current_script,
-            API_STR2PTR(cfg_file),
+            API_STR2PTR(config_file),
             name,
             SvIV (ST (2)), /* user_can_add_options */
             SvIV (ST (3)), /* user_can_delete_options */
@@ -5400,6 +5459,7 @@ weechat_perl_api_init (pTHX)
     API_DEF_FUNC(list_remove_all);
     API_DEF_FUNC(list_free);
     API_DEF_FUNC(config_new);
+    API_DEF_FUNC(config_set_version);
     API_DEF_FUNC(config_new_section);
     API_DEF_FUNC(config_search_section);
     API_DEF_FUNC(config_new_option);

commit 94cbed25853bcb4d43a4468eb9ee0cc3f42b0595

core: fix memory leak in function config_file_read_internal
This was caused by commit 66571a0b634ef28307f6d6ceef1ba29c091ca524.

src/core/wee-config-file.c
@@ -3346,9 +3346,12 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
     {
         line_number++;
 
+        option = NULL;
+        value = NULL;
+
         ptr_line = fgets (line, sizeof (line) - 1, config_file->file);
         if (!ptr_line)
-            continue;
+            goto end_line;
 
         /* encode line to internal charset */
         ptr_line2 = string_iconv_to_internal (NULL, ptr_line);
@@ -3374,7 +3377,7 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
 
         /* ignore empty line or comment */
         if (!ptr_line[0] || (ptr_line[0] == '#'))
-            continue;
+            goto end_line;
 
         /* beginning of section */
         if ((ptr_line[0] == '[') && !strchr (ptr_line, '='))
@@ -3409,12 +3412,8 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
                     free (section);
                 }
             }
-            continue;
+            goto end_line;
         }
-
-        /* option */
-        option = NULL;
-        value = NULL;
 
         /* skip escape char */
         if (ptr_line[0] == '\\')
@@ -3496,7 +3495,7 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
                                      config_file->version);
                 }
             }
-            continue;
+            goto end_line;
         }
 
         if (!ptr_section)
@@ -3507,7 +3506,7 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
                              gui_chat_prefix[GUI_CHAT_PREFIX_ERROR],
                              filename, line_number,
                              line);
-            continue;
+            goto end_line;
         }
 
         config_file_update_data_read (config_file,
@@ -3516,7 +3515,7 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
 
         /* option has been ignored by the update callback? */
         if (!option || !option[0])
-            continue;
+            goto end_line;
 
         if (ptr_section->callback_read)
         {
@@ -3576,6 +3575,7 @@ config_file_read_internal (struct t_config_file *config_file, int reload)
                 break;
         }
 
+    end_line:
         if (option)
             free (option);
         if (value)