2eaaa19bebaa02176393fecd7fa49d41b7bc39a8
Sébastien Helleu <flashcode@flashtux.org>
2023-04-15 23:52:29 +0200

    core: simplify print of stable/devel version number in version.sh

--- version.sh
+++ version.sh
@@ -65,14 +65,14 @@ case $1 in
     stable-major ) echo "${STABLE_MAJOR}" ;;
     stable-minor ) echo "${STABLE_MINOR}" ;;
     stable-patch ) echo "${STABLE_PATCH}" ;;
-    stable-number ) echo "0x$(printf "%02d" "${STABLE_MAJOR}")$(printf "%02d" "${STABLE_MINOR}")$(printf "%02d" "${STABLE_PATCH_DIGITS}")00" ;;
+    stable-number ) printf "0x%02d%02d%02d00\n" "${STABLE_MAJOR}" "${STABLE_MINOR}" "${STABLE_PATCH_DIGITS}" ;;
     # devel
     devel ) echo "${WEECHAT_DEVEL}" | cut -d"-" -f1 ;;
     devel-full ) echo "${WEECHAT_DEVEL}" ;;
     devel-major ) echo "${DEVEL_MAJOR}" ;;
     devel-minor ) echo "${DEVEL_MINOR}" ;;
     devel-patch ) echo "${DEVEL_PATCH}" ;;
-    devel-number ) echo "0x$(printf "%02d" "${DEVEL_MAJOR}")$(printf "%02d" "${DEVEL_MINOR}")$(printf "%02d" "${DEVEL_PATCH_DIGITS}")00" ;;
+    devel-number ) printf "0x%02d%02d%02d00\n" "${DEVEL_MAJOR}" "${DEVEL_MINOR}" "${DEVEL_PATCH_DIGITS}" ;;
     # error
     * ) echo >&2 "ERROR: unknown version."
         exit 1 ;;
