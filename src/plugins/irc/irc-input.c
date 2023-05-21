8abde49ba2dc156a23b192e7f87f6f7451efd1b9
Sébastien Helleu <flashcode@flashtux.org>
2023-05-21 22:22:03 +0200

    irc: fix order of tags in outgoing messages
    The order of tags in message doesn't matter, but this is for the sake of
    consistency with the other messages displayed.

--- src/plugins/irc/irc-input.c
+++ src/plugins/irc/irc-input.c
@@ -93,7 +93,7 @@ irc_input_user_message_display (struct t_gui_buffer *buffer, int action,
                 weechat_config_color (
                     weechat_config_get ("weechat.color.chat_nick_self")));
             snprintf (str_tags, sizeof (str_tags),
-                      "notify_none,self_msg,no_highlight,prefix_nick_%s",
+                      "self_msg,notify_none,no_highlight,prefix_nick_%s",
                       (str_color) ? str_color : "default");
             if (str_color)
                 free (str_color);
