36ea983d0494a1f9458f443da2989a9b745f50f4
Sébastien Helleu <flashcode@flashtux.org>
2023-07-12 20:47:52 +0200

    Version 4.0.3-dev

--- version.sh
+++ version.sh
@@ -40,7 +40,7 @@
 #
 
 WEECHAT_STABLE="4.0.2"
-WEECHAT_DEVEL="4.0.2"
+WEECHAT_DEVEL="4.0.3-dev"
 
 STABLE_MAJOR=$(echo "${WEECHAT_STABLE}" | cut -d"." -f1)
 STABLE_MINOR=$(echo "${WEECHAT_STABLE}" | cut -d"." -f2)
