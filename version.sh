f3bc66b25574aada02dcdbf884b7126e71be85fb
Sébastien Helleu <flashcode@flashtux.org>
2023-08-08 20:51:54 +0200

    Version 4.0.3

--- version.sh
+++ version.sh
@@ -39,8 +39,8 @@
 #     devel-number   the devel version as hex number ("0x04010000" for "4.1.0-dev")
 #
 
-WEECHAT_STABLE="4.0.2"
-WEECHAT_DEVEL="4.0.3-dev"
+WEECHAT_STABLE="4.0.3"
+WEECHAT_DEVEL="4.0.3"
 
 STABLE_MAJOR=$(echo "${WEECHAT_STABLE}" | cut -d"." -f1)
 STABLE_MINOR=$(echo "${WEECHAT_STABLE}" | cut -d"." -f2)
