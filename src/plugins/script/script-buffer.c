weechat/weechat

commit 817d1eaf8e0be700bf9b61dcbaf9db7ab507d3af

script: add local key bindings during the buffer creation
This allows the user to bind or unbind keys by setting options
"weechat.buffer.script.scripts.key_bind_*" and
"weechat.buffer.script.scripts.key_unbind_*".

src/plugins/script/script-buffer.c
@@ -1085,10 +1085,13 @@ script_buffer_set_callbacks ()
 
 /*
  * Sets keys on script buffer.
+ *
+ * If hashtable is not NULL, it is used to set keys, otherwise keys are directly
+ * set in the script buffer.
  */
 
 void
-script_buffer_set_keys ()
+script_buffer_set_keys (struct t_hashtable *hashtable)
 {
     char *keys[][2] = {
         { "meta-A", "toggleautoload" },
@@ -1113,12 +1116,18 @@ script_buffer_set_keys ()
         {
             snprintf (str_key, sizeof (str_key), "key_bind_%s", keys[i][0]);
             snprintf (str_command, sizeof (str_command), "/script %s", keys[i][1]);
-            weechat_buffer_set (script_buffer, str_key, str_command);
+            if (hashtable)
+                weechat_hashtable_set (hashtable, str_key, str_command);
+            else
+                weechat_buffer_set (script_buffer, str_key, str_command);
         }
         else
         {
             snprintf (str_key, sizeof (str_key), "key_unbind_%s", keys[i][0]);
-            weechat_buffer_set (script_buffer, str_key, "");
+            if (hashtable)
+                weechat_hashtable_set (hashtable, str_key, "");
+            else
+                weechat_buffer_set (script_buffer, str_key, "");
         }
     }
 }
@@ -1159,6 +1168,7 @@ script_buffer_open ()
         weechat_hashtable_set (buffer_props, "type", "free");
         weechat_hashtable_set (buffer_props, "title", _("Scripts"));
         weechat_hashtable_set (buffer_props, "localvar_set_type", "script");
+        script_buffer_set_keys (buffer_props);
     }
 
     script_buffer = weechat_buffer_new_props (
@@ -1173,7 +1183,6 @@ script_buffer_open ()
     if (!script_buffer)
         return;
 
-    script_buffer_set_keys ();
     script_buffer_set_localvar_filter ();
 
     script_buffer_selected_line = 0;

src/plugins/script/script-buffer.h
@@ -46,7 +46,7 @@ extern int script_buffer_input_cb (const void *pointer, void *data,
 extern int script_buffer_close_cb (const void *pointer, void *data,
                                    struct t_gui_buffer *buffer);
 extern void script_buffer_set_callbacks ();
-extern void script_buffer_set_keys ();
+extern void script_buffer_set_keys (struct t_hashtable *hashtable);
 extern void script_buffer_set_localvar_filter ();
 extern void script_buffer_open ();
 

src/plugins/script/script-config.c
@@ -279,7 +279,7 @@ script_config_change_use_keys_cb (const void *pointer, void *data,
     (void) option;
 
     if (script_buffer)
-        script_buffer_set_keys ();
+        script_buffer_set_keys (NULL);
 }
 
 /*

commit 2c251cd285cc33e5a94cd4cd84f81f3f91c407cf

script: fix up/down keys on /script buffer
Regression was introduced in commit 817d1eaf8e0be700bf9b61dcbaf9db7ab507d3af.

src/plugins/script/script-buffer.c
@@ -1094,6 +1094,8 @@ void
 script_buffer_set_keys (struct t_hashtable *hashtable)
 {
     char *keys[][2] = {
+        { "up",     "up"             },
+        { "down",   "down"           },
         { "meta-A", "toggleautoload" },
         { "meta-l", "load"           },
         { "meta-u", "unload"         },
@@ -1108,11 +1110,11 @@ script_buffer_set_keys (struct t_hashtable *hashtable)
     char str_key[64], str_command[64];
     int i;
 
-    weechat_buffer_set (script_buffer, "key_bind_up", "/script up");
-    weechat_buffer_set (script_buffer, "key_bind_down", "/script down");
     for (i = 0; keys[i][0]; i++)
     {
-        if (weechat_config_boolean (script_config_look_use_keys))
+        if (weechat_config_boolean (script_config_look_use_keys)
+            || (strcmp (keys[i][1], "up") == 0)
+            || (strcmp (keys[i][1], "down") == 0))
         {
             snprintf (str_key, sizeof (str_key), "key_bind_%s", keys[i][0]);
             snprintf (str_command, sizeof (str_command), "/script %s", keys[i][1]);