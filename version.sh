8c3b7b6c6ee851c86d8e1907c243017935136283
Sébastien Helleu <flashcode@flashtux.org>
2023-12-03 19:24:40 +0100

    core: add versions 4.0.7 and 4.1.2 in ChangeLog and release notes

--- version.sh
+++ version.sh
@@ -39,7 +39,7 @@
 #     devel-number   the devel version as hex number ("0x04010000" for "4.1.0-dev")
 #
 
-weechat_stable="4.1.1"
+weechat_stable="4.1.2"
 weechat_devel="4.2.0-dev"
 
 stable_major=$(echo "${weechat_stable}" | cut -d"." -f1)
