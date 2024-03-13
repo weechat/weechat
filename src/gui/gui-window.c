weechat/weechat

commit b83b428c5cc48043cb625844b87e94acbc194ba1

core: add incremental search in commands history (issue #2040)
Changes:

- move key ctrl+r to ctrl+s
- add key ctrl+r to search in commands history
- add option `search_history` in command `/input`
- add key context "histsearch"
- add option weechat.look.buffer_search_history
- add buffer variables "text_search_direction", "text_search_history" and "text_search_ptr_history"

src/gui/gui-window.c
@@ -50,6 +50,7 @@
 #include "gui-color.h"
 #include "gui-filter.h"
 #include "gui-input.h"
+#include "gui-history.h"
 #include "gui-hotlist.h"
 #include "gui-layout.h"
 #include "gui-line.h"
@@ -1501,8 +1502,7 @@ gui_window_scroll_previous_highlight (struct t_gui_window *window)
     if (!window)
         return;
 
-    if ((window->buffer->type == GUI_BUFFER_TYPE_FORMATTED)
-        && (window->buffer->text_search == GUI_TEXT_SEARCH_DISABLED))
+    if (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED)
     {
         if (window->buffer->lines->first_line)
         {
@@ -1539,8 +1539,7 @@ gui_window_scroll_next_highlight (struct t_gui_window *window)
     if (!window)
         return;
 
-    if ((window->buffer->type == GUI_BUFFER_TYPE_FORMATTED)
-        && (window->buffer->text_search == GUI_TEXT_SEARCH_DISABLED))
+    if (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED)
     {
         if (window->buffer->lines->first_line)
         {
@@ -1575,38 +1574,35 @@ gui_window_scroll_unread (struct t_gui_window *window)
     if (!window)
         return;
 
-    if (window->buffer->text_search == GUI_TEXT_SEARCH_DISABLED)
+    if (CONFIG_STRING(config_look_read_marker) &&
+        CONFIG_STRING(config_look_read_marker)[0] &&
+        (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED) &&
+        (window->buffer->lines->first_line_not_read ||
+         (window->buffer->lines->last_read_line &&
+          window->buffer->lines->last_read_line != window->buffer->lines->last_line)))
     {
-        if (CONFIG_STRING(config_look_read_marker) &&
-            CONFIG_STRING(config_look_read_marker)[0] &&
-            (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED) &&
-            (window->buffer->lines->first_line_not_read ||
-             (window->buffer->lines->last_read_line &&
-              window->buffer->lines->last_read_line != window->buffer->lines->last_line)))
+        if (window->buffer->lines->first_line_not_read)
+            window->scroll->start_line = window->buffer->lines->first_line;
+        else
+            window->scroll->start_line = window->buffer->lines->last_read_line->next_line;
+        if (window->scroll->start_line)
         {
-            if (window->buffer->lines->first_line_not_read)
-                window->scroll->start_line = window->buffer->lines->first_line;
-            else
-                window->scroll->start_line = window->buffer->lines->last_read_line->next_line;
-            if (window->scroll->start_line)
-            {
-                if (!gui_line_is_displayed (window->scroll->start_line))
-                    window->scroll->start_line = gui_line_get_next_displayed (window->scroll->start_line);
-            }
-            window->scroll->start_line_pos = 0;
-            window->scroll->first_line_displayed =
-                (window->scroll->start_line == gui_line_get_first_displayed (window->buffer));
-            gui_buffer_ask_chat_refresh (window->buffer, 2);
+            if (!gui_line_is_displayed (window->scroll->start_line))
+                window->scroll->start_line = gui_line_get_next_displayed (window->scroll->start_line);
         }
+        window->scroll->start_line_pos = 0;
+        window->scroll->first_line_displayed =
+            (window->scroll->start_line == gui_line_get_first_displayed (window->buffer));
+        gui_buffer_ask_chat_refresh (window->buffer, 2);
     }
 }
 
 /*
- * Searches for text in a buffer.
+ * Searches for text in buffer lines or commands history.
  *
  * Returns:
- *   1: line has been found with text
- *   0: no line found with text
+ *   1: successful search
+ *   0: no results found
  */
 
 int
@@ -1617,51 +1613,66 @@ gui_window_search_text (struct t_gui_window *window)
     if (!window)
         return 0;
 
-    if (window->buffer->text_search == GUI_TEXT_SEARCH_BACKWARD)
+    switch (window->buffer->text_search)
     {
-        if (window->buffer->lines->first_line
-            && window->buffer->input_buffer && window->buffer->input_buffer[0])
-        {
-            ptr_line = (window->scroll->start_line) ?
-                gui_line_get_prev_displayed (window->scroll->start_line) :
-                gui_line_get_last_displayed (window->buffer);
-            while (ptr_line)
+        case GUI_BUFFER_SEARCH_DISABLED:
+            break;
+        case GUI_BUFFER_SEARCH_LINES:
+            if (window->buffer->text_search_direction == GUI_BUFFER_SEARCH_DIR_BACKWARD)
             {
-                if (gui_line_search_text (window->buffer, ptr_line))
+                if (window->buffer->lines->first_line
+                    && window->buffer->input_buffer && window->buffer->input_buffer[0])
                 {
-                    window->scroll->start_line = ptr_line;
-                    window->scroll->start_line_pos = 0;
-                    window->scroll->first_line_displayed =
-                        (window->scroll->start_line == gui_line_get_first_displayed (window->buffer));
-                    gui_buffer_ask_chat_refresh (window->buffer, 2);
-                    return 1;
+                    ptr_line = (window->scroll->start_line) ?
+                        gui_line_get_prev_displayed (window->scroll->start_line) :
+                        gui_line_get_last_displayed (window->buffer);
+                    while (ptr_line)
+                    {
+                        if (gui_line_search_text (window->buffer, ptr_line))
+                        {
+                            window->scroll->start_line = ptr_line;
+                            window->scroll->start_line_pos = 0;
+                            window->scroll->first_line_displayed =
+                                (window->scroll->start_line == gui_line_get_first_displayed (window->buffer));
+                            gui_buffer_ask_chat_refresh (window->buffer, 2);
+                            return 1;
+                        }
+                        ptr_line = gui_line_get_prev_displayed (ptr_line);
+                    }
                 }
-                ptr_line = gui_line_get_prev_displayed (ptr_line);
             }
-        }
-    }
-    else if (window->buffer->text_search == GUI_TEXT_SEARCH_FORWARD)
-    {
-        if (window->buffer->lines->first_line
-            && window->buffer->input_buffer && window->buffer->input_buffer[0])
-        {
-            ptr_line = (window->scroll->start_line) ?
-                gui_line_get_next_displayed (window->scroll->start_line) :
-                gui_line_get_first_displayed (window->buffer);
-            while (ptr_line)
+            else if (window->buffer->text_search_direction == GUI_BUFFER_SEARCH_DIR_FORWARD)
             {
-                if (gui_line_search_text (window->buffer, ptr_line))
+                if (window->buffer->lines->first_line
+                    && window->buffer->input_buffer && window->buffer->input_buffer[0])
                 {
-                    window->scroll->start_line = ptr_line;
-                    window->scroll->start_line_pos = 0;
-                    window->scroll->first_line_displayed =
-                        (window->scroll->start_line == window->buffer->lines->first_line);
-                    gui_buffer_ask_chat_refresh (window->buffer, 2);
-                    return 1;
+                    ptr_line = (window->scroll->start_line) ?
+                        gui_line_get_next_displayed (window->scroll->start_line) :
+                        gui_line_get_first_displayed (window->buffer);
+                    while (ptr_line)
+                    {
+                        if (gui_line_search_text (window->buffer, ptr_line))
+                        {
+                            window->scroll->start_line = ptr_line;
+                            window->scroll->start_line_pos = 0;
+                            window->scroll->first_line_displayed =
+                                (window->scroll->start_line == window->buffer->lines->first_line);
+                            gui_buffer_ask_chat_refresh (window->buffer, 2);
+                            return 1;
+                        }
+                        ptr_line = gui_line_get_next_displayed (ptr_line);
+                    }
                 }
-                ptr_line = gui_line_get_next_displayed (ptr_line);
             }
-        }
+            break;
+        case GUI_BUFFER_SEARCH_HISTORY:
+            return gui_history_search (
+                window->buffer,
+                (window->buffer->text_search_history == GUI_BUFFER_SEARCH_HISTORY_LOCAL) ?
+                window->buffer->history : gui_history);
+            break;
+        case GUI_BUFFER_NUM_SEARCH:
+            break;
     }
     return 0;
 }
@@ -1672,45 +1683,75 @@ gui_window_search_text (struct t_gui_window *window)
  */
 
 void
-gui_window_search_start (struct t_gui_window *window,
+gui_window_search_start (struct t_gui_window *window, int search,
                          struct t_gui_line *text_search_start_line)
 {
     if (!window)
         return;
 
-    window->scroll->text_search_start_line = text_search_start_line;
-    window->buffer->text_search =
-        (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED) ?
-        GUI_TEXT_SEARCH_BACKWARD : GUI_TEXT_SEARCH_FORWARD;
+    window->buffer->text_search = search;
 
-    if ((window->buffer->text_search_where == 0)
-        ||  CONFIG_BOOLEAN(config_look_buffer_search_force_default))
+    switch (window->buffer->text_search)
     {
-        /* set default search values */
-        window->buffer->text_search_exact = CONFIG_BOOLEAN(config_look_buffer_search_case_sensitive);
-        window->buffer->text_search_regex = CONFIG_BOOLEAN(config_look_buffer_search_regex);
-        if (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED)
-        {
-            switch (CONFIG_ENUM(config_look_buffer_search_where))
+        case GUI_BUFFER_SEARCH_DISABLED:
+            break;
+        case GUI_BUFFER_SEARCH_LINES:
+            window->buffer->text_search_direction = (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED) ?
+                GUI_BUFFER_SEARCH_DIR_BACKWARD : GUI_BUFFER_SEARCH_DIR_FORWARD;
+            window->scroll->text_search_start_line = text_search_start_line;
+            if ((window->buffer->text_search_where == 0)
+                || CONFIG_BOOLEAN(config_look_buffer_search_force_default))
             {
-                case CONFIG_LOOK_BUFFER_SEARCH_PREFIX:
-                    window->buffer->text_search_where = GUI_TEXT_SEARCH_IN_PREFIX;
-                    break;
-                case CONFIG_LOOK_BUFFER_SEARCH_MESSAGE:
-                    window->buffer->text_search_where = GUI_TEXT_SEARCH_IN_MESSAGE;
-                    break;
-                case CONFIG_LOOK_BUFFER_SEARCH_PREFIX_MESSAGE:
-                    window->buffer->text_search_where = GUI_TEXT_SEARCH_IN_PREFIX | GUI_TEXT_SEARCH_IN_MESSAGE;
-                    break;
-                default:
-                    window->buffer->text_search_where = GUI_TEXT_SEARCH_IN_MESSAGE;
-                    break;
-            }
+                if (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED)
+                {
+                    switch (CONFIG_ENUM(config_look_buffer_search_where))
+                    {
+                        case CONFIG_LOOK_BUFFER_SEARCH_PREFIX:
+                            window->buffer->text_search_where = GUI_BUFFER_SEARCH_IN_PREFIX;
+                            break;
+                        case CONFIG_LOOK_BUFFER_SEARCH_MESSAGE:
+                            window->buffer->text_search_where = GUI_BUFFER_SEARCH_IN_MESSAGE;
+                            break;
+                        case CONFIG_LOOK_BUFFER_SEARCH_PREFIX_MESSAGE:
+                            window->buffer->text_search_where = GUI_BUFFER_SEARCH_IN_PREFIX
+                                | GUI_BUFFER_SEARCH_IN_MESSAGE;
+                            break;
+                        default:
+                            window->buffer->text_search_where = GUI_BUFFER_SEARCH_IN_MESSAGE;
+                            break;
+                    }
+                }
+                else
+                {
+                    window->buffer->text_search_where = GUI_BUFFER_SEARCH_IN_MESSAGE;
+                }
+                break;
+            case GUI_BUFFER_SEARCH_HISTORY:
+                window->buffer->text_search_direction = GUI_BUFFER_SEARCH_DIR_BACKWARD;
+                if ((window->buffer->text_search_history == GUI_BUFFER_SEARCH_HISTORY_NONE)
+                    || CONFIG_BOOLEAN(config_look_buffer_search_force_default))
+                {
+                    switch (CONFIG_ENUM(config_look_buffer_search_history))
+                    {
+                        case CONFIG_LOOK_BUFFER_SEARCH_HISTORY_LOCAL:
+                            window->buffer->text_search_history = GUI_BUFFER_SEARCH_HISTORY_LOCAL;
+                            break;
+                        case CONFIG_LOOK_BUFFER_SEARCH_HISTORY_GLOBAL:
+                            window->buffer->text_search_history = GUI_BUFFER_SEARCH_HISTORY_GLOBAL;
+                            break;
+                        default:
+                            window->buffer->text_search_history = GUI_BUFFER_SEARCH_HISTORY_LOCAL;
+                            break;
+                    }
+                }
+                break;
+            case GUI_BUFFER_NUM_SEARCH:
+                break;
         }
-        else
-            window->buffer->text_search_where = GUI_TEXT_SEARCH_IN_MESSAGE;
     }
 
+    window->buffer->text_search_exact = CONFIG_BOOLEAN(config_look_buffer_search_case_sensitive);
+    window->buffer->text_search_regex = CONFIG_BOOLEAN(config_look_buffer_search_regex);
     window->buffer->text_search_found = 0;
     gui_input_search_compile_regex (window->buffer);
     if (window->buffer->text_search_input)
@@ -1734,39 +1775,80 @@ gui_window_search_restart (struct t_gui_window *window)
     if (!window)
         return;
 
-    window->scroll->start_line = window->scroll->text_search_start_line;
-    window->scroll->start_line_pos = 0;
-    window->buffer->text_search =
-        (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED) ?
-        GUI_TEXT_SEARCH_BACKWARD : GUI_TEXT_SEARCH_FORWARD;
-    window->buffer->text_search_found = 0;
-    gui_input_search_compile_regex (window->buffer);
-    if (gui_window_search_text (window))
-        window->buffer->text_search_found = 1;
-    else
+    switch (window->buffer->text_search)
     {
-        if (CONFIG_BOOLEAN(config_look_search_text_not_found_alert)
-            && window->buffer->input_buffer && window->buffer->input_buffer[0])
-        {
-            fprintf (stderr, "\a");
-            fflush (stderr);
-        }
-        gui_buffer_ask_chat_refresh (window->buffer, 2);
+        case GUI_BUFFER_SEARCH_DISABLED:
+            break;
+        case GUI_BUFFER_SEARCH_LINES:
+            window->scroll->start_line = window->scroll->text_search_start_line;
+            window->scroll->start_line_pos = 0;
+            window->buffer->text_search_direction =
+                (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED) ?
+                GUI_BUFFER_SEARCH_DIR_BACKWARD : GUI_BUFFER_SEARCH_DIR_FORWARD;
+            gui_input_search_compile_regex (window->buffer);
+            window->buffer->text_search_found = 0;
+            if (gui_window_search_text (window))
+            {
+                window->buffer->text_search_found = 1;
+            }
+            else
+            {
+                if (CONFIG_BOOLEAN(config_look_search_text_not_found_alert)
+                    && window->buffer->input_buffer && window->buffer->input_buffer[0])
+                {
+                    fprintf (stderr, "\a");
+                    fflush (stderr);
+                }
+                gui_buffer_ask_chat_refresh (window->buffer, 2);
+            }
+            break;
+        case GUI_BUFFER_SEARCH_HISTORY:
+            gui_input_search_compile_regex (window->buffer);
+            window->buffer->text_search_found = 0;
+            window->buffer->text_search_ptr_history = NULL;
+            if (gui_window_search_text (window))
+            {
+                window->buffer->text_search_found = 1;
+            }
+            else
+            {
+                if (CONFIG_BOOLEAN(config_look_search_text_not_found_alert)
+                    && window->buffer->input_buffer && window->buffer->input_buffer[0])
+                {
+                    fprintf (stderr, "\a");
+                    fflush (stderr);
+                }
+            }
+            break;
+        case GUI_BUFFER_NUM_SEARCH:
+            break;
     }
 }
 
 /*
- * Ends search mode in a buffer (helper function).
+ * Stops search in a buffer, at current position if stop_here == 1 or reset
+ * scroll to the initial value if stop_here == 0.
  */
 
 void
-gui_window_search_end (struct t_gui_window *window)
+gui_window_search_stop (struct t_gui_window *window, int stop_here)
 {
+    const char *ptr_new_input;
+    int search;
+
     if (!window)
         return;
 
-    window->buffer->text_search = GUI_TEXT_SEARCH_DISABLED;
-    window->buffer->text_search = 0;
+    search = window->buffer->text_search;
+
+    ptr_new_input = (stop_here
+                     && (window->buffer->text_search == GUI_BUFFER_SEARCH_HISTORY)
+                     && window->buffer->text_search_ptr_history
+                     && window->buffer->text_search_ptr_history->text) ?
+        window->buffer->text_search_ptr_history->text : window->buffer->text_search_input;
+
+    window->buffer->text_search = GUI_BUFFER_SEARCH_DISABLED;
+    window->buffer->text_search_direction = GUI_BUFFER_SEARCH_DIR_BACKWARD;
     if (window->buffer->text_search_regex_compiled)
     {
         regfree (window->buffer->text_search_regex_compiled);
@@ -1774,49 +1856,31 @@ gui_window_search_end (struct t_gui_window *window)
         window->buffer->text_search_regex_compiled = NULL;
     }
     gui_input_delete_line (window->buffer);
-    if (window->buffer->text_search_input)
+    if (ptr_new_input)
     {
-        gui_input_insert_string (window->buffer,
-                                 window->buffer->text_search_input);
+        gui_input_insert_string (window->buffer, ptr_new_input);
         gui_input_text_changed_modifier_and_signal (window->buffer,
                                                     0, /* save undo */
                                                     1); /* stop completion */
+    }
+    if (window->buffer->text_search_input)
+    {
         free (window->buffer->text_search_input);
         window->buffer->text_search_input = NULL;
     }
-}
+    window->buffer->text_search_ptr_history = NULL;
 
-/*
- * Stops search in a buffer at current position.
- */
-
-void
-gui_window_search_stop_here (struct t_gui_window *window)
-{
-    if (!window)
-        return;
-
-    gui_window_search_end (window);
-    window->scroll->text_search_start_line = NULL;
-    gui_buffer_ask_chat_refresh (window->buffer, 2);
-}
-
-/*
- * Stops search in a buffer.
- */
-
-void
-gui_window_search_stop (struct t_gui_window *window)
-{
-    if (!window)
-        return;
-
-    gui_window_search_end (window);
-    window->scroll->start_line = window->scroll->text_search_start_line;
-    window->scroll->start_line_pos = 0;
-    window->scroll->text_search_start_line = NULL;
-    gui_hotlist_remove_buffer (window->buffer, 0);
-    gui_buffer_ask_chat_refresh (window->buffer, 2);
+    if (search == GUI_BUFFER_SEARCH_LINES)
+    {
+        window->scroll->text_search_start_line = NULL;
+        if (!stop_here)
+        {
+            window->scroll->start_line = window->scroll->text_search_start_line;
+            window->scroll->start_line_pos = 0;
+            gui_hotlist_remove_buffer (window->buffer, 0);
+        }
+        gui_buffer_ask_chat_refresh (window->buffer, 2);
+    }
 }
 
 /*

src/core/wee-command.c
@@ -3525,6 +3525,8 @@ COMMAND_CALLBACK(input)
         gui_input_search_text_here (buffer);
     else if (string_strcmp (argv[1], "search_text") == 0)
         gui_input_search_text (buffer);
+    else if (string_strcmp (argv[1], "search_history") == 0)
+        gui_input_search_history (buffer);
     else if (string_strcmp (argv[1], "search_previous") == 0)
         gui_input_search_previous (buffer);
     else if (string_strcmp (argv[1], "search_next") == 0)
@@ -8481,6 +8483,7 @@ command_init ()
             N_("> raw[complete_previous]: complete word with previous completion"),
             N_("> raw[search_text_here]: search text in buffer at current position"),
             N_("> raw[search_text]: search text in buffer"),
+            N_("> raw[search_history]: search text in command line history"),
             N_("> raw[search_switch_case]: switch exact case for search"),
             N_("> raw[search_switch_regex]: switch search type: string/regular expression"),
             N_("> raw[search_switch_where]: switch search in messages/prefixes"),
@@ -8535,9 +8538,9 @@ command_init ()
             N_("This command is used by key bindings or plugins.")),
         "return || split_return || "
         "complete_next || complete_previous || search_text_here || "
-        "search_text || search_switch_case || search_switch_regex || "
-        "search_switch_where || search_previous || search_next || "
-        "search_stop_here || search_stop || "
+        "search_text || search_history || search_switch_case || "
+        "search_switch_regex || search_switch_where || search_previous || "
+        "search_next || search_stop_here || search_stop || "
         "delete_previous_char || delete_next_char || delete_previous_word || "
         "delete_previous_word_whitespace || delete_next_word || "
         "delete_beginning_of_line || delete_beginning_of_input || "

src/core/wee-config.c
@@ -89,7 +89,7 @@ struct t_config_section *weechat_config_section_buffer = NULL;
 struct t_config_section *weechat_config_section_notify = NULL;
 struct t_config_section *weechat_config_section_filter = NULL;
 struct t_config_section *weechat_config_section_key[GUI_KEY_NUM_CONTEXTS] = {
-    NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,
 };
 
 /* config, startup section */
@@ -114,6 +114,7 @@ struct t_config_option *config_look_buffer_auto_renumber = NULL;
 struct t_config_option *config_look_buffer_notify_default = NULL;
 struct t_config_option *config_look_buffer_position = NULL;
 struct t_config_option *config_look_buffer_search_case_sensitive = NULL;
+struct t_config_option *config_look_buffer_search_history = NULL;
 struct t_config_option *config_look_buffer_search_force_default = NULL;
 struct t_config_option *config_look_buffer_search_regex = NULL;
 struct t_config_option *config_look_buffer_search_where = NULL;
@@ -3166,6 +3167,14 @@ config_weechat_init_options ()
             "buffer_search_case_sensitive", "boolean",
             N_("default text search in buffer: case sensitive or not"),
             NULL, 0, 0, "off", NULL, 0,
+            NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+        config_look_buffer_search_history = config_file_new_option (
+            weechat_config_file, weechat_config_section_look,
+            "buffer_search_history", "enum",
+            N_("default text search command line history: local (buffer) or "
+               "global history"),
+            "local|global", 0, 0, "local",
+            NULL, 0,
             NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
         config_look_buffer_search_force_default = config_file_new_option (
             weechat_config_file, weechat_config_section_look,

src/core/wee-config.h
@@ -50,6 +50,12 @@ enum t_config_look_buffer_position
     CONFIG_LOOK_BUFFER_POSITION_FIRST_GAP,
 };
 
+enum t_config_look_buffer_search_history
+{
+    CONFIG_LOOK_BUFFER_SEARCH_HISTORY_LOCAL = 0,
+    CONFIG_LOOK_BUFFER_SEARCH_HISTORY_GLOBAL,
+};
+
 enum t_config_look_buffer_search_where
 {
     CONFIG_LOOK_BUFFER_SEARCH_PREFIX = 0,
@@ -165,6 +171,7 @@ extern struct t_config_option *config_look_buffer_auto_renumber;
 extern struct t_config_option *config_look_buffer_notify_default;
 extern struct t_config_option *config_look_buffer_position;
 extern struct t_config_option *config_look_buffer_search_case_sensitive;
+extern struct t_config_option *config_look_buffer_search_history;
 extern struct t_config_option *config_look_buffer_search_force_default;
 extern struct t_config_option *config_look_buffer_search_regex;
 extern struct t_config_option *config_look_buffer_search_where;

src/core/wee-upgrade.c
@@ -546,16 +546,7 @@ upgrade_weechat_read_buffer (struct t_infolist *infolist)
         }
     }
 
-    /* text search */
-    ptr_buffer->text_search = infolist_integer (infolist, "text_search");
-    ptr_buffer->text_search_exact = infolist_integer (infolist,
-                                                      "text_search_exact");
-    ptr_buffer->text_search_found = infolist_integer (infolist,
-                                                      "text_search_found");
-    if (ptr_buffer->text_search_input)
-        free (ptr_buffer->text_search_input);
-    str = infolist_string (infolist, "text_search_input");
-    ptr_buffer->text_search_input = (str) ? strdup (str) : NULL;
+    /* text search is disabled after upgrade */
 
     /* highlight options */
     gui_buffer_set_highlight_words (

src/gui/curses/gui-curses-chat.c
@@ -1505,8 +1505,8 @@ gui_chat_display_line (struct t_gui_window *window, struct t_gui_line *line,
         ptr_data = (message_with_tags) ?
             message_with_tags : line->data->message;
         message_with_search = NULL;
-        if ((window->buffer->text_search != GUI_TEXT_SEARCH_DISABLED)
-            && (window->buffer->text_search_where & GUI_TEXT_SEARCH_IN_MESSAGE)
+        if ((window->buffer->text_search == GUI_BUFFER_SEARCH_LINES)
+            && (window->buffer->text_search_where & GUI_BUFFER_SEARCH_IN_MESSAGE)
             && (!window->buffer->text_search_regex
                 || window->buffer->text_search_regex_compiled))
         {
@@ -1695,7 +1695,7 @@ gui_chat_display_line (struct t_gui_window *window, struct t_gui_line *line,
     else
     {
         /* display marker if line is matching user search */
-        if (window->buffer->text_search != GUI_TEXT_SEARCH_DISABLED)
+        if (window->buffer->text_search == GUI_BUFFER_SEARCH_LINES)
         {
             if (gui_line_search_text (window->buffer, line))
             {
@@ -1765,8 +1765,8 @@ gui_chat_display_line_y (struct t_gui_window *window, struct t_gui_line *line,
     }
 
     /* emphasize text (if searching text) */
-    if ((window->buffer->text_search != GUI_TEXT_SEARCH_DISABLED)
-        && (window->buffer->text_search_where & GUI_TEXT_SEARCH_IN_MESSAGE)
+    if ((window->buffer->text_search == GUI_BUFFER_SEARCH_LINES)
+        && (window->buffer->text_search_where & GUI_BUFFER_SEARCH_IN_MESSAGE)
         && (!window->buffer->text_search_regex
             || window->buffer->text_search_regex_compiled))
     {

commit edc566ffe8950833f993f566f3512107709b011d

core: make search_stop scroll to search start position
When you run /input search_stop it should scroll to the scroll position
the buffer was at before starting the search, rather than to the bottom
of the buffer.

Fixes a regression introduced in commit b83b428c5cc48043cb625844b87e94acbc194ba1

Fixes #2093

src/gui/gui-window.c
@@ -1872,13 +1872,13 @@ gui_window_search_stop (struct t_gui_window *window, int stop_here)
 
     if (search == GUI_BUFFER_SEARCH_LINES)
     {
-        window->scroll->text_search_start_line = NULL;
         if (!stop_here)
         {
             window->scroll->start_line = window->scroll->text_search_start_line;
             window->scroll->start_line_pos = 0;
             gui_hotlist_remove_buffer (window->buffer, 0);
         }
+        window->scroll->text_search_start_line = NULL;
         gui_buffer_ask_chat_refresh (window->buffer, 2);
     }
 }