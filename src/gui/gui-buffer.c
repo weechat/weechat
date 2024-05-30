weechat/weechat

commit 7addd1bf0070c8b6c36f6e1d90245c44f742d7ee

core: add function gui_buffer_set_input

src/gui/gui-buffer.c
@@ -2274,6 +2274,23 @@ gui_buffer_remove_hotlist_max_level_nicks (struct t_gui_buffer *buffer,
 }
 
 /*
+ * Sets buffer input.
+ */
+
+void
+gui_buffer_set_input (struct t_gui_buffer *buffer, const char *input)
+{
+    if (!buffer || (string_strcmp (buffer->input_buffer, input) == 0))
+        return;
+
+    gui_buffer_undo_snap (buffer);
+    gui_input_replace_input (buffer, input);
+    gui_input_text_changed_modifier_and_signal (buffer,
+                                                1, /* save undo */
+                                                1); /* stop completion */
+}
+
+/*
  * Sets flag "input_get_any_user_data" for a buffer.
  */
 
@@ -2677,11 +2694,7 @@ gui_buffer_set (struct t_gui_buffer *buffer, const char *property,
     }
     else if (strcmp (property, "input") == 0)
     {
-        gui_buffer_undo_snap (buffer);
-        gui_input_replace_input (buffer, value);
-        gui_input_text_changed_modifier_and_signal (buffer,
-                                                    1, /* save undo */
-                                                    1); /* stop completion */
+        gui_buffer_set_input (buffer, value);
     }
     else if (strcmp (property, "input_pos") == 0)
     {

tests/unit/gui/test-gui-buffer.cpp
@@ -1209,6 +1209,16 @@ TEST(GuiBuffer, RemoveHotlistMaxLevelNicks)
 
 /*
  * Tests functions:
+ *   gui_buffer_set_input
+ */
+
+TEST(GuiBuffer, SetInput)
+{
+    /* TODO: write tests */
+}
+
+/*
+ * Tests functions:
  *   gui_buffer_set_input_get_any_user_data
  */
 

commit 5e751cafaeb4d9daaf1b181310e3d4e936b276f7

core: always send input_text_changed signal on buffer set input
When the buffer input is set to the same string it already has, we
should still run the input modifiers and send the input_text_changed
signal. This is because plugins/scripts may rely on them being run when
they set the input.

An example is the go.py script. When you start it, it stores the input
and sets it to an empty string, and uses some input modifiers to display
custom data in the input field. Since this function just returned early
if the input was empty, it had a bug where if you had /go bound to a key
and pressed the key when you had an empty input, the buffers go adds to
the input wouldn't be shown until you press another key.

This fixes a regression that was introduced in commit
7addd1bf0070c8b6c36f6e1d90245c44f742d7ee.

src/gui/gui-buffer.c
@@ -2353,7 +2353,7 @@ gui_buffer_set_input_prompt (struct t_gui_buffer *buffer,
 void
 gui_buffer_set_input (struct t_gui_buffer *buffer, const char *input)
 {
-    if (!buffer || (string_strcmp (buffer->input_buffer, input) == 0))
+    if (!buffer)
         return;
 
     gui_buffer_undo_snap (buffer);