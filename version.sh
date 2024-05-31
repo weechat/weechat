82f4f83f2e729b711f27f131086adb2063eef030
Sébastien Helleu <flashcode@flashtux.org>
2024-05-31 13:17:00 +0200

    Version 4.2.3

--- version.sh
+++ version.sh
@@ -39,8 +39,8 @@
 #     devel-number   the devel version as hex number ("0x04010000" for "4.1.0-dev")
 #
 
-weechat_stable="4.2.2"
-weechat_devel="4.2.3-dev"
+weechat_stable="4.2.3"
+weechat_devel="4.2.3"
 
 stable_major=$(echo "${weechat_stable}" | cut -d"." -f1)
 stable_minor=$(echo "${weechat_stable}" | cut -d"." -f2)
