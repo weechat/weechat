8923ecc7d83d42794b903531a46a75cc256d0a55
Sébastien Helleu <flashcode@flashtux.org>
2024-06-14 18:10:45 +0200

    core: replace "%lx" by "%p" in function relay_weechat_msg_add_pointer

--- src/plugins/relay/weechat/relay-weechat-msg.c
+++ src/plugins/relay/weechat/relay-weechat-msg.c
@@ -245,8 +245,7 @@ relay_weechat_msg_add_pointer (struct t_relay_weechat_msg *msg, void *pointer)
     char str_pointer[128];
     unsigned char length;
 
-    snprintf (str_pointer, sizeof (str_pointer),
-              "%lx", (unsigned long)pointer);
+    snprintf (str_pointer, sizeof (str_pointer), "%p", pointer);
     length = strlen (str_pointer);
     relay_weechat_msg_add_bytes (msg, &length, 1);
     relay_weechat_msg_add_bytes (msg, str_pointer, length);
