weechat/weechat

commit 3d041a0364c06b0c19cbd88fcf037b68138699a6

ruby: fix builtin functions not available (issue #2109)

src/plugins/ruby/weechat-ruby.c
@@ -1266,6 +1266,7 @@ weechat_plugin_init (struct t_weechat_plugin *plugin, int argc, char *argv[])
 {
     int ruby_error, old_ruby_quiet;
     VALUE err;
+    char* process_options_argv[] = { "ruby", "-enil", NULL };
     char *weechat_ruby_code = {
         "$stdout = WeechatOutputs\n"
         "$stderr = WeechatOutputs\n"
@@ -1363,6 +1364,8 @@ weechat_plugin_init (struct t_weechat_plugin *plugin, int argc, char *argv[])
 #endif
 
     ruby_init ();
+
+    ruby_process_options (2, process_options_argv);
 
     /* redirect stdin and stdout */
     ruby_mWeechatOutputs = rb_define_module ("WeechatOutputs");

commit 7cb991667c56a5a020d113db9b04006d705d8db1

ruby: fix crash in plugin initialization (issue #2163)
This regression was introduced by commit
3d041a0364c06b0c19cbd88fcf037b68138699a6.

src/plugins/ruby/weechat-ruby.c
@@ -1266,7 +1266,7 @@ weechat_plugin_init (struct t_weechat_plugin *plugin, int argc, char *argv[])
 {
     int ruby_error, old_ruby_quiet;
     VALUE err;
-    char* process_options_argv[] = { "ruby", "-enil", NULL };
+    char* ruby_options_argv[] = { "ruby", "-enil", NULL };
     char *weechat_ruby_code = {
         "$stdout = WeechatOutputs\n"
         "$stderr = WeechatOutputs\n"
@@ -1365,7 +1365,7 @@ weechat_plugin_init (struct t_weechat_plugin *plugin, int argc, char *argv[])
 
     ruby_init ();
 
-    ruby_process_options (2, process_options_argv);
+    ruby_options (2, ruby_options_argv);
 
     /* redirect stdin and stdout */
     ruby_mWeechatOutputs = rb_define_module ("WeechatOutputs");