d1013b31bd4cbe9a14cb2b86b534e0215643deca
Sébastien Helleu <flashcode@flashtux.org>
2024-10-30 08:00:00 +0100

    build: fix check of WeeChat git repository
    The `.git` directory can also be a regular file in a git worktree.

--- tools/set_git_version.sh
+++ tools/set_git_version.sh
@@ -46,7 +46,7 @@ git_version=""
 case ${version} in
 *-*)
     # devel/rc version (like 0.4.0-dev or 0.4.0-rc1)
-    if [ -d "${root_dir}/.git" ]; then
+    if [ -e "${root_dir}/.git" ]; then
         git_version=$(cd "${root_dir}" && git describe 2>/dev/null)
     fi
     ;;
