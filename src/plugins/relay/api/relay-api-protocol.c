weechat/weechat

commit 93ec10b563718e5c2eb59457ce6685e57a53274a

relay/api: return HTTP error 405 (Method Not Allowed) when the method received is not allowed

src/plugins/relay/api/relay-api-protocol.c
@@ -374,7 +374,7 @@ RELAY_API_PROTOCOL_CALLBACK(options)
     relay_api_msg_send_json (
         client,
         RELAY_HTTP_204_NO_CONTENT,
-        "Access-Control-Allow-Methods: GET, POST, PUT, DELETE\r\n"
+        "Access-Control-Allow-Methods: " RELAY_API_ALLOWED_METHODS "\r\n"
         "Access-Control-Allow-Headers: origin, content-type, accept, authorization",
         NULL,  /* body_type */
         NULL);  /* json_body */
@@ -1197,7 +1197,7 @@ relay_api_protocol_recv_json (struct t_relay_client *client, const char *json)
 void
 relay_api_protocol_recv_http (struct t_relay_client *client)
 {
-    int i, num_args;
+    int i, num_args, match_method, match_resource;
     char str_error[1024];
     enum t_relay_api_protocol_rc return_code;
     struct t_relay_api_protocol_cb protocol_cb[] = {
@@ -1238,18 +1238,22 @@ relay_api_protocol_recv_http (struct t_relay_client *client)
         || !client->http_req->path_items[1]
         || (strcmp (client->http_req->path_items[0], "api") != 0))
     {
-        goto error_not_found;
+        goto resource_not_found;
     }
 
     num_args = client->http_req->num_path_items - 2;
 
     for (i = 0; protocol_cb[i].resource; i++)
     {
-        if (strcmp (protocol_cb[i].method, client->http_req->method) != 0)
-            continue;
+        match_method = (strcmp (protocol_cb[i].method, client->http_req->method) == 0);
 
-        if ((strcmp (protocol_cb[i].resource, "*") != 0)
-            && (strcmp (protocol_cb[i].resource, client->http_req->path_items[1]) != 0))
+        match_resource = ((strcmp (protocol_cb[i].resource, "*") == 0)
+                          || (strcmp (protocol_cb[i].resource, client->http_req->path_items[1]) == 0));
+
+        if (!match_method && (strcmp (protocol_cb[i].resource, "*") != 0) && match_resource)
+            goto error_method_not_allowed;
+
+        if (!match_method || !match_resource)
             continue;
 
         if (protocol_cb[i].auth_required
@@ -1331,7 +1335,18 @@ relay_api_protocol_recv_http (struct t_relay_client *client)
         }
     }
 
-    goto error_not_found;
+resource_not_found:
+    if ((strcmp (client->http_req->method, "GET") != 0)
+        && (strcmp (client->http_req->method, "POST") != 0)
+        && (strcmp (client->http_req->method, "PUT") != 0)
+        && (strcmp (client->http_req->method, "DELETE") != 0))
+    {
+        goto error_method_not_allowed;
+    }
+    else
+    {
+        goto error_not_found;
+    }
 
 error_bad_request:
     relay_api_msg_send_error_json (client, RELAY_HTTP_400_BAD_REQUEST,
@@ -1341,6 +1356,12 @@ error_bad_request:
 error_not_found:
     relay_api_msg_send_error_json (client, RELAY_HTTP_404_NOT_FOUND,
                                    NULL, RELAY_HTTP_ERROR_NOT_FOUND);
+    goto error;
+
+error_method_not_allowed:
+    relay_api_msg_send_error_json (client, RELAY_HTTP_405_METHOD_NOT_ALLOWED,
+                                   "Allow: " RELAY_API_ALLOWED_METHODS,
+                                   RELAY_HTTP_ERROR_METHOD_NOT_ALLOWED);
     goto error;
 
 error_memory:

src/plugins/relay/api/relay-api-protocol.h
@@ -22,6 +22,8 @@
 #ifndef WEECHAT_PLUGIN_RELAY_API_PROTOCOL_H
 #define WEECHAT_PLUGIN_RELAY_API_PROTOCOL_H
 
+#define RELAY_API_ALLOWED_METHODS "GET, POST, PUT, DELETE"
+
 #define RELAY_API_CB(__command) &relay_api_protocol_cb_##__command
 #define RELAY_API_PROTOCOL_CALLBACK(__command)                          \
     enum t_relay_api_protocol_rc                                        \

src/plugins/relay/relay-http.h
@@ -40,6 +40,7 @@ enum t_relay_client_http_status
 #define RELAY_HTTP_401_UNAUTHORIZED           401, "Unauthorized"
 #define RELAY_HTTP_403_FORBIDDEN              403, "Forbidden"
 #define RELAY_HTTP_404_NOT_FOUND              404, "Not Found"
+#define RELAY_HTTP_405_METHOD_NOT_ALLOWED     405, "Method Not Allowed"
 #define RELAY_HTTP_500_INTERNAL_SERVER_ERROR  500, "Internal Server Error"
 #define RELAY_HTTP_503_SERVICE_UNAVAILABLE    503, "Service Unavailable"
 
@@ -53,6 +54,7 @@ enum t_relay_client_http_status
 #define RELAY_HTTP_ERROR_INVALID_ITERATIONS   "Invalid number of iterations"
 #define RELAY_HTTP_ERROR_BAD_REQUEST          "Bad request"
 #define RELAY_HTTP_ERROR_NOT_FOUND            "Resource not found"
+#define RELAY_HTTP_ERROR_METHOD_NOT_ALLOWED   "Method Not Allowed"
 #define RELAY_HTTP_ERROR_OUT_OF_MEMORY        "Out of memory"
 
 struct t_relay_http_request

commit bff910cae373f404a429a7cc810e734808aabd58

relay/api: fix crash when an invalid HTTP request is received from a client
When invalid data is received (not an HTTP request), client->http_req->method
is NULL, so we have to check it's not NULL before comparing it to the supported
methods.

This fixes a regression introduced in commit
93ec10b563718e5c2eb59457ce6685e57a53274a.

po/cs.po
@@ -23,7 +23,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-05-04 09:24+0200\n"
 "Last-Translator: Ondřej Súkup <mimi.vx@gmail.com>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -14024,6 +14024,10 @@ msgstr ""
 #, fuzzy, c-format
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "%s%s: selhalo parsování příkazu \"%s\" (prosím oznamte to vývojárům):"
+
+#, fuzzy, c-format
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr "%s%s: TLS handshake selhal"
 
 #, fuzzy, c-format
 #| msgid "%s%s: unable to create socket"

po/de.po
@@ -28,7 +28,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-05-05 09:24+0200\n"
 "Last-Translator: Nils Görs <weechatter@arcor.de>\n"
 "Language-Team: German <kde-i18n-de@kde.org>\n"
@@ -15418,6 +15418,11 @@ msgstr ""
 #, c-format
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "%s%s: Route „%s %s“ für Client %s%s%s konnte nicht ausgeführt werden"
+
+#, fuzzy, c-format
+#| msgid "%s%s: invalid websocket handshake received for client %s%s%s"
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr "%s%s: ungültigen WebSocket Handshake für Client %s%s%s empfangen"
 
 #, c-format
 msgid "%sremote[%s]: unable to send data, disconnecting"

po/es.po
@@ -24,7 +24,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: Santiago Forero <santiago@forero.xyz>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -14358,6 +14358,10 @@ msgstr ""
 #| msgid "%s%s: failed to execute command \"%s\" for client %s%s%s"
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "%s%s: falló la ejecución del comando \"%s\" por el cliente %s%s%s"
+
+#, fuzzy, c-format
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr "%s%s: agotado el tiempo de espera del saludo TLS del cliente %s%s%s"
 
 #, fuzzy, c-format
 #| msgid "%s%s: unable to create socket"

po/fr.po
@@ -23,8 +23,8 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
-"PO-Revision-Date: 2025-06-30 09:34+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
+"PO-Revision-Date: 2025-08-16 19:27+0200\n"
 "Last-Translator: Sébastien Helleu <flashcode@flashtux.org>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
 "Language: fr\n"
@@ -15115,6 +15115,10 @@ msgstr ""
 #, c-format
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "%s%s : échec d'exécution de la route \"%s %s\" pour le client %s%s%s"
+
+#, c-format
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr "%s%s : données invalides reçues du client %s%s%s"
 
 #, c-format
 msgid "%sremote[%s]: unable to send data, disconnecting"

po/hu.po
@@ -22,7 +22,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: Andras Voroskoi <voroskoi@frugalware.org>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -13423,6 +13423,10 @@ msgstr ""
 #, fuzzy, c-format
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "Nem sikerült a(z) \"%s\" naplófájlt írni\n"
+
+#, fuzzy, c-format
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr "%s gnutls kézfogás sikertelen\n"
 
 #, fuzzy, c-format
 msgid "%sremote[%s]: unable to send data, disconnecting"

po/it.po
@@ -22,7 +22,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: Esteban I. Ruiz Moreno <exio4.com@gmail.com>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -14360,6 +14360,10 @@ msgstr ""
 #| msgid "%s%s: failed to execute command \"%s\" for client %s%s%s"
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "%s%s: impossibile eseguire il comando \"%s\" per il client %s%s%s"
+
+#, fuzzy, c-format
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr "%s%s: set di caratter invalido: \"%s\""
 
 #, fuzzy, c-format
 #| msgid "%s%s: unable to create socket"

po/ja.po
@@ -22,7 +22,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: AYANOKOUZI, Ryuunosuke <i38w7i3@yahoo.co.jp>\n"
 "Language-Team: Japanese <https://github.com/l/weechat/tree/master/"
@@ -14848,6 +14848,12 @@ msgstr ""
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr ""
 "%1$s%2$s: クライアント %4$s%5$s%6$s に対するコマンド \"%3$s\" の実行に失敗"
+
+#, fuzzy, c-format
+#| msgid "%s%s: invalid websocket handshake received for client %s%s%s"
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr ""
+"%s%s: クライアント %s%s%s から受け取った不正なウェブソケットハンドシェイク"
 
 #, fuzzy, c-format
 #| msgid "%s%s: unable to create socket"

po/pl.po
@@ -24,7 +24,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: Krzysztof Korościk <soltys@soltys.info>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -15044,6 +15044,12 @@ msgstr ""
 #, c-format
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "%s%s: nie udało się wykonać trasy „%s %s” dla klienta %s%s%s"
+
+#, fuzzy, c-format
+#| msgid "%s%s: invalid websocket handshake received for client %s%s%s"
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr ""
+"%s%s: otrzymano niepoprawny pakiet dla gniazda webowego od klienta %s%s%s"
 
 #, fuzzy, c-format
 #| msgid "%sremote[%s]: unable to create socket"

po/pt.po
@@ -22,7 +22,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: Vasco Almeida <vascomalmeida@sapo.pt>\n"
 "Language-Team: Portuguese <>\n"
@@ -14733,6 +14733,11 @@ msgstr ""
 #| msgid "%s%s: failed to execute command \"%s\" for client %s%s%s"
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "%s%s: falha ao executar o comando \"%s\" do cliente %s%s%s"
+
+#, fuzzy, c-format
+#| msgid "%s%s: invalid websocket handshake received for client %s%s%s"
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr "%s%s: websocket handshake inválido recebido do cliente %s%s%s"
 
 #, fuzzy, c-format
 #| msgid "%s%s: unable to create socket"

po/pt_BR.po
@@ -23,7 +23,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: Érico Nogueira <ericonr@disroot.org>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -13856,6 +13856,10 @@ msgstr ""
 #, fuzzy, c-format
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "%s%s: falha ao executar comando \"%s\" para cliente %d"
+
+#, fuzzy, c-format
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr "%s%s: handshake TLS falhou"
 
 #, fuzzy, c-format
 msgid "%sremote[%s]: unable to send data, disconnecting"

po/ru.po
@@ -23,7 +23,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: Aleksey V Zapparov AKA ixti <ixti@member.fsf.org>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -13475,6 +13475,10 @@ msgstr ""
 #, fuzzy, c-format
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "Не могу записать лог-файл \"%s\"\n"
+
+#, fuzzy, c-format
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr "%s инициализация gnutls не удалось\n"
 
 #, fuzzy, c-format
 msgid "%sremote[%s]: unable to send data, disconnecting"

po/sr.po
@@ -22,7 +22,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-07-09 15:48+0400\n"
 "Last-Translator: Ivan Pešić <ivan.pesic@gmail.com>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -14668,6 +14668,11 @@ msgstr ""
 #, c-format
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "%s%s: није успело извршавање руте „%s %s” за клијента %s%s%s"
+
+#, fuzzy, c-format
+#| msgid "%s%s: invalid websocket handshake received for client %s%s%s"
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr "%s%s: примљено је неважеће вебсокет руковање од клијента %s%s%s"
 
 #, c-format
 msgid "%sremote[%s]: unable to send data, disconnecting"

po/tr.po
@@ -23,7 +23,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2025-07-06 09:25+0200\n"
 "Last-Translator: Emir SARI <emir_sari@icloud.com>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -1466,10 +1466,11 @@ msgid ""
 "term|url|windows || callbacks <duration>[<unit>] || mouse|cursor [verbose] "
 "|| hdata [free] || time <command> || unicode <string> || whitespace"
 msgstr ""
-"list || set <eklenti> <düzey> || dump [<eklenti>] || hooks [<eklenti_maskesi> "
-"[<kanca_türü>...]] || buffer|certs|color|dirs|infolists|key|libs|memory|tags|"
-"term|url|windows || callbacks <süre>[<birim>] || mouse|cursor [verbose] "
-"|| hdata [free] || time <komut> || unicode <dizi> || whitespace"
+"list || set <eklenti> <düzey> || dump [<eklenti>] || hooks "
+"[<eklenti_maskesi> [<kanca_türü>...]] || buffer|certs|color|dirs|infolists|"
+"key|libs|memory|tags|term|url|windows || callbacks <süre>[<birim>] || mouse|"
+"cursor [verbose] || hdata [free] || time <komut> || unicode <dizi> || "
+"whitespace"
 
 msgid "raw[list]: list plugins with debug levels"
 msgstr "raw[list]: Eklentileri hata ayıklama düzeyleriyle listele"
@@ -1577,8 +1578,9 @@ msgid "raw[term]: display infos about terminal"
 msgstr "raw[term]: Uçbirim hakkında bilgi görüntüle"
 
 msgid "raw[url]: toggle debug for calls to hook_url (display output hashtable)"
-msgstr "raw[url]: hook_url'sine olan çağrılar için hata ayıklamayı aç/kapat "
-"(çıktı sağlama tablosunu görüntüle)"
+msgstr ""
+"raw[url]: hook_url'sine olan çağrılar için hata ayıklamayı aç/kapat (çıktı "
+"sağlama tablosunu görüntüle)"
 
 msgid "raw[windows]: display windows tree"
 msgstr "raw[windows]: Pencere ağacını görüntüle"
@@ -1586,8 +1588,9 @@ msgstr "raw[windows]: Pencere ağacını görüntüle"
 msgid ""
 "raw[time]: measure time to execute a command or to send text to the current "
 "buffer"
-msgstr "raw[time]: Geçerli arabelleğe metin göndermek veya üzerinde komut "
-"yürütmek için gereken zamanı ölç"
+msgstr ""
+"raw[time]: Geçerli arabelleğe metin göndermek veya üzerinde komut yürütmek "
+"için gereken zamanı ölç"
 
 msgid ""
 "raw[unicode]: display information about string and unicode chars (evaluated, "
@@ -1621,15 +1624,15 @@ msgid ""
 "raw[-s]: split expression before evaluating it (many commands can be "
 "separated by semicolons)"
 msgstr ""
-"raw[-s]: İfadeyi değerlendirmeden önce böl (birden çok komut noktalı virgülle "
-"ayrılabilir)"
+"raw[-s]: İfadeyi değerlendirmeden önce böl (birden çok komut noktalı "
+"virgülle ayrılabilir)"
 
 msgid ""
 "raw[-d]: display debug output after evaluation (with two -d: more verbose "
 "debug)"
 msgstr ""
-"raw[-d]: Değerlendirme sonrası hata ayıklama çıktısını görüntüle (iki -d ile: "
-"Daha ayrıntılı hata ayıklama bilgisi)"
+"raw[-d]: Değerlendirme sonrası hata ayıklama çıktısını görüntüle (iki -d "
+"ile: Daha ayrıntılı hata ayıklama bilgisi)"
 
 msgid ""
 "raw[-c]: evaluate as condition: use operators and parentheses, return a "
@@ -1641,7 +1644,8 @@ msgstr ""
 msgid ""
 "expression: expression to evaluate, variables with format ${variable} are "
 "replaced (see below)"
-msgstr "ifade: Değerlendirilecek ifade, ${variable} biçimindeki değişkenler "
+msgstr ""
+"ifade: Değerlendirilecek ifade, ${variable} biçimindeki değişkenler "
 "değerlendirilir (aşağıya bakın)"
 
 msgid "operator: a logical or comparison operator (see below)"
@@ -1650,7 +1654,8 @@ msgstr "işleç: bir mantıksal veya karşılaştırma işleci (aşağıya bakı
 msgid ""
 "An expression is considered as \"true\" if it is not NULL, not empty, and "
 "different from \"0\"."
-msgstr "Bir ifade; NULL veya boş değilse ve \"0\"dan farklıysa \"true\" olarak "
+msgstr ""
+"Bir ifade; NULL veya boş değilse ve \"0\"dan farklıysa \"true\" olarak "
 "değerlendirilir."
 
 msgid ""
@@ -1928,16 +1933,17 @@ msgid "  !~   is NOT matching POSIX extended regex"
 msgstr "  !~   eşleşen POSIX genişletilmiş düzenli ifadesi DEĞİL"
 
 msgid "  ==*  is matching mask, case-sensitive (wildcard \"*\" is allowed)"
-msgstr "  ==*  eşleşen maske, BÜYÜK/küçük harf duyarlı (\"*\" jokerine izin "
-"verilir)"
+msgstr ""
+"  ==*  eşleşen maske, BÜYÜK/küçük harf duyarlı (\"*\" jokerine izin verilir)"
 
 msgid "  !!*  is NOT matching mask, case-sensitive (wildcard \"*\" is allowed)"
-msgstr "  !!*  eşleşen maske DEĞİL, BÜYÜK/küçük harf duyarlı (\"*\" jokerine "
-"izin verilir)"
+msgstr ""
+"  !!*  eşleşen maske DEĞİL, BÜYÜK/küçük harf duyarlı (\"*\" jokerine izin "
+"verilir)"
 
 msgid "  =*   is matching mask, case-insensitive (wildcard \"*\" is allowed)"
-msgstr "  =*   eşleşen maske, BÜYÜK/küçük harf duyarsız (\"*\" jokerine izin "
-"verilir)"
+msgstr ""
+"  =*   eşleşen maske, BÜYÜK/küçük harf duyarsız (\"*\" jokerine izin verilir)"
 
 msgid ""
 "  !*   is NOT matching mask, case-insensitive (wildcard \"*\" is allowed)"
@@ -2389,9 +2395,8 @@ msgid ""
 "<name> <new_name> || refresh <name>|<mask>... || recreate <name> || del "
 "<name>|<mask>..."
 msgstr ""
-"list || add|addreplace <ad> \"<koşullar>\" \"<içerik>\" || rename "
-"<ad> <yeni_ad> || refresh <ad>|<maske>... || recreate <ad> || del "
-"<ad>|<maske>..."
+"list || add|addreplace <ad> \"<koşullar>\" \"<içerik>\" || rename <ad> "
+"<yeni_ad> || refresh <ad>|<maske>... || recreate <ad> || del <ad>|<maske>..."
 
 msgid "raw[list]: list all custom bar items"
 msgstr "raw[list]: Tüm özel çubuk ögelerini listele"
@@ -2408,7 +2413,8 @@ msgstr "ad: Özel çubuk ögesi adı"
 msgid ""
 "conditions: evaluated conditions to display the bar item (for example to "
 "display the bar item only in specific buffers)"
-msgstr "koşullar: Çubuk ögesini görüntülemek için değerlendirilmiş koşullar "
+msgstr ""
+"koşullar: Çubuk ögesini görüntülemek için değerlendirilmiş koşullar "
 "(örneğin, çubuk ögesini yalnızca belirli arabelleklerde görüntülemek için)"
 
 msgid "content: content (evaluated, see /help eval)"
@@ -2467,9 +2473,9 @@ msgid ""
 msgstr ""
 "[list|listdefault|listdiff] [<içerik>] || bind <düğme> [<komut> "
 "[<argümanlar>]] || bindctxt <içerik> <düğme> [<komut> [<argümanlar>]] || "
-"unbind <düğme> || unbindctxt <içerik> <düğme> || reset <düğme> || "
-"resetctxt <içerik> <düğme> || resetall -yes [<içerik>] || missing [<içerik>] "
-"|| legacy <düğme>..."
+"unbind <düğme> || unbindctxt <içerik> <düğme> || reset <düğme> || resetctxt "
+"<içerik> <düğme> || resetall -yes [<içerik>] || missing [<içerik>] || legacy "
+"<düğme>..."
 
 msgid "raw[list]: list all current keys"
 msgstr ""
@@ -4714,12 +4720,12 @@ msgid ""
 "messages from nick \"FlashCode\", \"irc_notice+nick_toto*\" for notices from "
 "a nick starting with \"toto\""
 msgstr ""
-"vurgulanacak künyelerin virgülle ayrılmış listesi; BÜYÜK/küçük harf "
-"duyarsız karşılaştırma; her bir künyede \"*\" jokerine izin verilir; "
-"künyeler arasında mantıksal \"and\" yapmak için künyeleri \"+\" ile "
-"ayırabilirsiniz; örnekler: \"GazMan\" takma adından iletiler için "
-"\"nick_gazman\", \"toto\" ile başlayan bir takma addan gelecek bildirimler "
-"için \"irc_notice+nick_toto\""
+"vurgulanacak künyelerin virgülle ayrılmış listesi; BÜYÜK/küçük harf duyarsız "
+"karşılaştırma; her bir künyede \"*\" jokerine izin verilir; künyeler "
+"arasında mantıksal \"and\" yapmak için künyeleri \"+\" ile ayırabilirsiniz; "
+"örnekler: \"GazMan\" takma adından iletiler için \"nick_gazman\", \"toto\" "
+"ile başlayan bir takma addan gelecek bildirimler için "
+"\"irc_notice+nick_toto\""
 
 msgid ""
 "conditions to add a buffer in hotlist (if notify level is OK for the "
@@ -11969,8 +11975,8 @@ msgid ""
 "allowed in tags; an empty value allows highlight on any tag"
 msgstr ""
 "vurgulamayı irc arabelleklerindeki bu künyelere sınırla (kullanıcı "
-"iletilerinde vurgulama olup sunucu iletilerinde olmaması için); künyeler "
-"bir virgülle ayrılmalıdır ve \"+\" künyeler arasında mantıksal bir \"and\" "
+"iletilerinde vurgulama olup sunucu iletilerinde olmaması için); künyeler bir "
+"virgülle ayrılmalıdır ve \"+\" künyeler arasında mantıksal bir \"and\" "
 "yapmak için kullanılabilir; künyelerde \"*\" jokerine izin verilir; boş bir "
 "değer herhangi bir künyede vurgulamaya izin verir"
 
@@ -12701,17 +12707,17 @@ msgid ""
 "\"channel\" was not found), \"pos_text\": index of \"text\" message (\"-1\" "
 "if \"text\" was not found)"
 msgstr ""
-"\"tags\": künyeler, \"tag_xxx\": künyenin kaçırılmamış değeri \"xxx\" "
-"(künye başına bir anahtar), \"message_without_tags\": künyesiz ileti, "
-"\"nick\": takma ad, \"user\": kullanıcı adı, \"host\": makine, \"command\": "
-"komut, \"channel\": kanal, \"arguments\": argümanlar (kanalı içerir), "
-"\"text\": metin (örneğin: kullanıcı iletisi), \"param1\" ... \"paramN\": "
-"ayrıştırılmış komut parametreleri, \"num_params\": ayrıştırılmış komut "
-"parametrelerinin sayısı, \"pos_command\": \"command\" iletisinin indeksi "
-"(komut bulunamazsa \"-1\"), \"pos_arguments\": \"arguments\" iletisinin "
-"indeksi (\"arguments\" bulunamazsa \"-1\"), \"pos_channel\": \"channel\" "
-"iletisinin indeksi (\"channel\" bulunamazsa \"-1\"), \"pos_text\":  \"text\" "
-"iletisinin indeksi (\"text\" bulunamazsa \"-1\")"
+"\"tags\": künyeler, \"tag_xxx\": künyenin kaçırılmamış değeri \"xxx\" (künye "
+"başına bir anahtar), \"message_without_tags\": künyesiz ileti, \"nick\": "
+"takma ad, \"user\": kullanıcı adı, \"host\": makine, \"command\": komut, "
+"\"channel\": kanal, \"arguments\": argümanlar (kanalı içerir), \"text\": "
+"metin (örneğin: kullanıcı iletisi), \"param1\" ... \"paramN\": ayrıştırılmış "
+"komut parametreleri, \"num_params\": ayrıştırılmış komut parametrelerinin "
+"sayısı, \"pos_command\": \"command\" iletisinin indeksi (komut bulunamazsa "
+"\"-1\"), \"pos_arguments\": \"arguments\" iletisinin indeksi (\"arguments\" "
+"bulunamazsa \"-1\"), \"pos_channel\": \"channel\" iletisinin indeksi "
+"(\"channel\" bulunamazsa \"-1\"), \"pos_text\":  \"text\" iletisinin indeksi "
+"(\"text\" bulunamazsa \"-1\")"
 
 msgid "split an IRC message (to fit in 512 bytes by default)"
 msgstr "bir IRC iletisini böl (öntanımlı olarak 512 bayt'a sığması için)"
@@ -14937,6 +14943,11 @@ msgstr ""
 #| msgid "%s%s: failed to execute command \"%s\" for client %s%s%s"
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
 msgstr "%s%s: \"%s\" komutu yürütülemedi, %s%s%s istemcisi için"
+
+#, fuzzy, c-format
+#| msgid "%s%s: invalid websocket handshake received for client %s%s%s"
+msgid "%s%s: invalid data received from client %s%s%s"
+msgstr "%s%s: %s%s%s istemcisi için geçersiz web soketi el sıkışması alındı"
 
 #, fuzzy, c-format
 #| msgid "%s%s: unable to create socket"

po/weechat.pot
@@ -23,7 +23,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-06-30 09:32+0200\n"
+"POT-Creation-Date: 2025-08-16 19:26+0200\n"
 "PO-Revision-Date: 2014-08-16 10:27+0200\n"
 "Last-Translator: Sébastien Helleu <flashcode@flashtux.org>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -12257,6 +12257,10 @@ msgstr ""
 
 #, c-format
 msgid "%s%s: failed to execute route \"%s %s\" for client %s%s%s"
+msgstr ""
+
+#, c-format
+msgid "%s%s: invalid data received from client %s%s%s"
 msgstr ""
 
 #, c-format

src/plugins/relay/api/relay-api-protocol.c
@@ -1356,10 +1356,11 @@ relay_api_protocol_recv_http (struct t_relay_client *client)
     }
 
 resource_not_found:
-    if ((strcmp (client->http_req->method, "GET") != 0)
-        && (strcmp (client->http_req->method, "POST") != 0)
-        && (strcmp (client->http_req->method, "PUT") != 0)
-        && (strcmp (client->http_req->method, "DELETE") != 0))
+    if (!client->http_req->method
+        || ((strcmp (client->http_req->method, "GET") != 0)
+            && (strcmp (client->http_req->method, "POST") != 0)
+            && (strcmp (client->http_req->method, "PUT") != 0)
+            && (strcmp (client->http_req->method, "DELETE") != 0)))
     {
         goto error_method_not_allowed;
     }
@@ -1392,15 +1393,28 @@ error_memory:
 error:
     if (weechat_relay_plugin->debug >= 1)
     {
-        weechat_printf (NULL,
-                        _("%s%s: failed to execute route \"%s %s\" "
-                          "for client %s%s%s"),
-                        weechat_prefix ("error"),
-                        RELAY_PLUGIN_NAME,
-                        client->http_req->method,
-                        client->http_req->path,
-                        RELAY_COLOR_CHAT_CLIENT,
-                        client->desc,
-                        RELAY_COLOR_CHAT);
+        if (client->http_req->method && client->http_req->path)
+        {
+            weechat_printf (NULL,
+                            _("%s%s: failed to execute route \"%s %s\" "
+                              "for client %s%s%s"),
+                            weechat_prefix ("error"),
+                            RELAY_PLUGIN_NAME,
+                            client->http_req->method,
+                            client->http_req->path,
+                            RELAY_COLOR_CHAT_CLIENT,
+                            client->desc,
+                            RELAY_COLOR_CHAT);
+        }
+        else
+        {
+            weechat_printf (NULL,
+                            _("%s%s: invalid data received from client %s%s%s"),
+                            weechat_prefix ("error"),
+                            RELAY_PLUGIN_NAME,
+                            RELAY_COLOR_CHAT_CLIENT,
+                            client->desc,
+                            RELAY_COLOR_CHAT);
+        }
     }
 }