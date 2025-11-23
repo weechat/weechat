93947c8e2d00ab551e80cba21bd2d3227c9d450c
Sébastien Helleu <flashcode@flashtux.org>
2025-11-23 11:38:49 +0100

    Version 4.7.3-dev

--- version.sh
+++ version.sh
@@ -42,7 +42,7 @@
 #
 
 weechat_stable="4.7.2"
-weechat_devel="4.7.2"
+weechat_devel="4.7.3-dev"
 
 stable_major=$(echo "${weechat_stable}" | cut -d"." -f1)
 stable_minor=$(echo "${weechat_stable}" | cut -d"." -f2)
