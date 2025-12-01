weechat/weechat

commit 1b669cd13cd543e80868dac17c2c4a6ad7bf4432

irc: fix warning on creation of irc.msgbuffer option when the server name contains upper case letters (closes #2281)
Now the following command is OK without warning:

  /set irc.msgbuffer.TEST.notice current

And the following command returns an error instead of a warning (that means the
option is NOT created):

  /set irc.msgbuffer.TEST.NOTICE current

src/plugins/irc/irc-config.c
@@ -1580,7 +1580,7 @@ irc_config_msgbuffer_create_option_cb (const void *pointer, void *data,
                                        const char *option_name, const char *value)
 {
     struct t_config_option *ptr_option;
-    char *name_lower;
+    char *pos, *name_lower;
     int rc;
 
     /* make C compiler happy */
@@ -1607,29 +1607,37 @@ irc_config_msgbuffer_create_option_cb (const void *pointer, void *data,
         {
             if (value)
             {
-                name_lower = weechat_string_tolower (option_name);
-                if (name_lower && (strcmp (option_name, name_lower) != 0))
+                pos = strrchr (option_name, '.');
+                if (pos)
                 {
-                    weechat_printf (
-                        NULL,
-                        _("%s%s: warning: the command name \"%s\" must be "
-                          "lower case, the option \"irc.msgbuffer.%s\" will "
-                          "not work"),
-                        weechat_prefix ("error"), IRC_PLUGIN_NAME,
-                        option_name, option_name);
+                    pos++;
+                    name_lower = weechat_string_tolower (pos);
+                    if (name_lower && (strcmp (pos, name_lower) == 0))
+                    {
+                        ptr_option = weechat_config_new_option (
+                            config_file, section,
+                            option_name, "enum",
+                            _("buffer used to display message received from IRC "
+                              "server"),
+                            "weechat|server|current|private", 0, 0, value, value, 0,
+                            NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+                        rc = (ptr_option) ?
+                            WEECHAT_CONFIG_OPTION_SET_OK_SAME_VALUE :
+                            WEECHAT_CONFIG_OPTION_SET_ERROR;
+                    }
+                    else
+                    {
+                        weechat_printf (
+                            NULL,
+                            _("%s%s: error: invalid option \"%s.%s\", the command "
+                              "name or alias \"%s\" must be lower case"),
+                            weechat_prefix ("error"), IRC_PLUGIN_NAME,
+                            "irc.msgbuffer", option_name, pos);
+                        free (name_lower);
+                        return rc;
+                    }
+                    free (name_lower);
                 }
-                free (name_lower);
-
-                ptr_option = weechat_config_new_option (
-                    config_file, section,
-                    option_name, "enum",
-                    _("buffer used to display message received from IRC "
-                      "server"),
-                    "weechat|server|current|private", 0, 0, value, value, 0,
-                    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-                rc = (ptr_option) ?
-                    WEECHAT_CONFIG_OPTION_SET_OK_SAME_VALUE :
-                    WEECHAT_CONFIG_OPTION_SET_ERROR;
             }
             else
                 rc = WEECHAT_CONFIG_OPTION_SET_OK_SAME_VALUE;
@@ -1640,8 +1648,9 @@ irc_config_msgbuffer_create_option_cb (const void *pointer, void *data,
     {
         weechat_printf (
             NULL,
-            _("%s%s: error creating \"%s\" => \"%s\""),
-            weechat_prefix ("error"), IRC_PLUGIN_NAME, option_name, value);
+            _("%s%s: error creating \"%s.%s\" => \"%s\""),
+            weechat_prefix ("error"), IRC_PLUGIN_NAME,
+            "irc.msgbuffer", option_name, value);
     }
 
     return rc;

po/cs.po
@@ -23,7 +23,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-10-12 17:27+0200\n"
+"POT-Creation-Date: 2025-10-14 22:54+0200\n"
 "PO-Revision-Date: 2025-05-04 09:24+0200\n"
 "Last-Translator: Ondřej Súkup <mimi.vx@gmail.com>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -10645,17 +10645,18 @@ msgstr ""
 "%s%s: varování: server \"%s\" nenalezen v konfiguračním souboru, nesmazáno z "
 "paměti, protože se stále používá"
 
-#, c-format
-msgid ""
-"%s%s: warning: the command name \"%s\" must be lower case, the option "
-"\"irc.msgbuffer.%s\" will not work"
-msgstr ""
-
 msgid "buffer used to display message received from IRC server"
 msgstr "buffer použitý pro zobrazení zprávy obdržené od IRC serveru"
 
 #, c-format
-msgid "%s%s: error creating \"%s\" => \"%s\""
+msgid ""
+"%s%s: error: invalid option \"%s.%s\", the command name or alias \"%s\" must "
+"be lower case"
+msgstr ""
+
+#, fuzzy, c-format
+#| msgid "%s%s: error creating \"%s\" => \"%s\""
+msgid "%s%s: error creating \"%s.%s\" => \"%s\""
 msgstr "%s%s: chyba vytváření \"%s\" => \"%s\""
 
 #, c-format

po/de.po
@@ -28,7 +28,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-10-12 17:27+0200\n"
+"POT-Creation-Date: 2025-10-14 22:54+0200\n"
 "PO-Revision-Date: 2025-05-05 09:24+0200\n"
 "Last-Translator: Nils Görs <weechatter@arcor.de>\n"
 "Language-Team: German <kde-i18n-de@kde.org>\n"
@@ -11722,20 +11722,19 @@ msgstr ""
 "gefunden werden. Da der Server zur Zeit genutzt wird, konnte er noch nicht "
 "entfernt werden"
 
-#, c-format
-msgid ""
-"%s%s: warning: the command name \"%s\" must be lower case, the option "
-"\"irc.msgbuffer.%s\" will not work"
-msgstr ""
-"%s%s: Warnung: Der Befehl \"%s\" muss in Kleinbuchstaben geschrieben werden, "
-"die Option \"irc.msgbuffer.%s\" funktioniert nicht"
-
 msgid "buffer used to display message received from IRC server"
 msgstr ""
 "Buffer welcher zur Ausgabe von IRC Server Nachrichten genutzt werden soll"
 
 #, c-format
-msgid "%s%s: error creating \"%s\" => \"%s\""
+msgid ""
+"%s%s: error: invalid option \"%s.%s\", the command name or alias \"%s\" must "
+"be lower case"
+msgstr ""
+
+#, fuzzy, c-format
+#| msgid "%s%s: error creating \"%s\" => \"%s\""
+msgid "%s%s: error creating \"%s.%s\" => \"%s\""
 msgstr "%s%s: Fehler beim Erstellen von \"%s\" => \"%s\""
 
 #, c-format
@@ -19046,3 +19045,11 @@ msgstr ""
 "%s%s Datei \"%s\" zum Fortsetzen der Übertragung wird nicht akzeptiert "
 "(Port: %d, Startposition: %llu): xfer nicht gefunden oder nicht bereit für "
 "Transfer"
+
+#, c-format
+#~ msgid ""
+#~ "%s%s: warning: the command name \"%s\" must be lower case, the option "
+#~ "\"irc.msgbuffer.%s\" will not work"
+#~ msgstr ""
+#~ "%s%s: Warnung: Der Befehl \"%s\" muss in Kleinbuchstaben geschrieben "
+#~ "werden, die Option \"irc.msgbuffer.%s\" funktioniert nicht"

po/es.po
@@ -24,7 +24,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-10-12 17:27+0200\n"
+"POT-Creation-Date: 2025-10-14 22:54+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: Santiago Forero <santiago@forero.xyz>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -10871,17 +10871,18 @@ msgstr ""
 "%s%s: atención: servidor \"%s\" no encontrado en el archivo de "
 "configuración, no es borrado de la memoria porque aún está en uso"
 
-#, c-format
-msgid ""
-"%s%s: warning: the command name \"%s\" must be lower case, the option "
-"\"irc.msgbuffer.%s\" will not work"
-msgstr ""
-
 msgid "buffer used to display message received from IRC server"
 msgstr "buffer donde mostrar los mensajes recibidos desde el servidor IRC"
 
 #, c-format
-msgid "%s%s: error creating \"%s\" => \"%s\""
+msgid ""
+"%s%s: error: invalid option \"%s.%s\", the command name or alias \"%s\" must "
+"be lower case"
+msgstr ""
+
+#, fuzzy, c-format
+#| msgid "%s%s: error creating \"%s\" => \"%s\""
+msgid "%s%s: error creating \"%s.%s\" => \"%s\""
 msgstr "%s%s: error al crear \"%s\" => \"%s\""
 
 #, c-format

po/fr.po
@@ -23,8 +23,8 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-10-12 17:27+0200\n"
-"PO-Revision-Date: 2025-10-12 17:30+0200\n"
+"POT-Creation-Date: 2025-10-14 22:54+0200\n"
+"PO-Revision-Date: 2025-10-14 22:54+0200\n"
 "Last-Translator: Sébastien Helleu <flashcode@flashtux.org>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
 "Language: fr\n"
@@ -11529,20 +11529,20 @@ msgstr ""
 "%s%s : attention : le serveur \"%s\" n'a pas été trouvé dans le fichier de "
 "configuration, non supprimé en mémoire car il est en cours d'utilisation"
 
-#, c-format
-msgid ""
-"%s%s: warning: the command name \"%s\" must be lower case, the option "
-"\"irc.msgbuffer.%s\" will not work"
-msgstr ""
-"%s%s : attention : le nom de commande \"%s\" doit être en minuscules, "
-"l'option \"irc.msgbuffer.%s\" ne fonctionnera pas"
-
 msgid "buffer used to display message received from IRC server"
 msgstr "tampon utilisé pour afficher le message reçu du serveur IRC"
 
 #, c-format
-msgid "%s%s: error creating \"%s\" => \"%s\""
-msgstr "%s%s : erreur de création \"%s\" => \"%s\""
+msgid ""
+"%s%s: error: invalid option \"%s.%s\", the command name or alias \"%s\" must "
+"be lower case"
+msgstr ""
+"%s%s : erreur : option \"%s.%s\" invalide, le nom de commande ou alias "
+"\"%s\" doit être en minuscules"
+
+#, c-format
+msgid "%s%s: error creating \"%s.%s\" => \"%s\""
+msgstr "%s%s : erreur de création \"%s.%s\" => \"%s\""
 
 #, c-format
 msgid ""
@@ -18684,3 +18684,11 @@ msgid ""
 msgstr ""
 "%s%s : impossible d'accepter de continuer le fichier \"%s\" (port : %d, "
 "position de départ : %llu) : xfer non trouvé ou non prêt pour le transfert"
+
+#, c-format
+#~ msgid ""
+#~ "%s%s: warning: the command name \"%s\" must be lower case, the option "
+#~ "\"irc.msgbuffer.%s\" will not work"
+#~ msgstr ""
+#~ "%s%s : attention : le nom de commande \"%s\" doit être en minuscules, "
+#~ "l'option \"irc.msgbuffer.%s\" ne fonctionnera pas"

po/hu.po
@@ -22,7 +22,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-10-12 17:27+0200\n"
+"POT-Creation-Date: 2025-10-14 22:54+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: Andras Voroskoi <voroskoi@frugalware.org>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -10204,17 +10204,17 @@ msgid ""
 "memory because it's currently used"
 msgstr ""
 
-#, c-format
-msgid ""
-"%s%s: warning: the command name \"%s\" must be lower case, the option "
-"\"irc.msgbuffer.%s\" will not work"
-msgstr ""
-
 msgid "buffer used to display message received from IRC server"
 msgstr ""
 
+#, c-format
+msgid ""
+"%s%s: error: invalid option \"%s.%s\", the command name or alias \"%s\" must "
+"be lower case"
+msgstr ""
+
 #, fuzzy, c-format
-msgid "%s%s: error creating \"%s\" => \"%s\""
+msgid "%s%s: error creating \"%s.%s\" => \"%s\""
 msgstr "%s nincs elég memória az információs pult üzenethez\n"
 
 #, c-format

po/it.po
@@ -22,7 +22,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-10-12 17:27+0200\n"
+"POT-Creation-Date: 2025-10-14 22:54+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: Esteban I. Ruiz Moreno <exio4.com@gmail.com>\n"
 "Language-Team: weechat-dev <weechat-dev@nongnu.org>\n"
@@ -10858,17 +10858,18 @@ msgstr ""
 "%s%s: attenzione: il server \"%s\" non è stato trovato nel file di "
 "configurazione, e non è stato eliminato perché è attivo"
 
-#, c-format
-msgid ""
-"%s%s: warning: the command name \"%s\" must be lower case, the option "
-"\"irc.msgbuffer.%s\" will not work"
-msgstr ""
-
 msgid "buffer used to display message received from IRC server"
 msgstr "buffer usato per visualizzare i messaggi ricevuti dal server IRC"
 
 #, c-format
-msgid "%s%s: error creating \"%s\" => \"%s\""
+msgid ""
+"%s%s: error: invalid option \"%s.%s\", the command name or alias \"%s\" must "
+"be lower case"
+msgstr ""
+
+#, fuzzy, c-format
+#| msgid "%s%s: error creating \"%s\" => \"%s\""
+msgid "%s%s: error creating \"%s.%s\" => \"%s\""
 msgstr "%s%s: errore nella creazione \"%s\" => \"%s\""
 
 #, c-format

po/ja.po
@@ -22,7 +22,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: WeeChat\n"
 "Report-Msgid-Bugs-To: flashcode@flashtux.org\n"
-"POT-Creation-Date: 2025-10-12 17:27+0200\n"
+"POT-Creation-Date: 2025-10-14 22:54+0200\n"
 "PO-Revision-Date: 2025-05-04 09:25+0200\n"
 "Last-Translator: AYANOKOUZI, Ryuunosuke <i38w7i3@yahoo.co.jp>\n"
 "Language-Team: Japanese <https://github.com/l/weechat/tree/master/"
@@ -11239,17 +11239,18 @@ msgstr ""
 "%s%s: 警告: サーバ \"%s\" が設定ファイルに見つかりません、現在使用中のためメ"
 "モリから削除されません"
 
-#, c-format
-msgid ""
-"%s%s: warning: the command name \"%s\" must be lower case, the option "
-"\"irc.msgbuffer.%s\" will not work"
-msgstr ""
-
 msgid "buffer used to display message received from IRC server"
 msgstr "IRC サーバから受け取ったメッセージを表示するバッファ"
 
 #, c-format
-msgid "%s%s: error creating \"%s\" => \"%s\""
+msgid ""
+"%s%s: error: invalid option \"%s.%s\", the command name or alias \"%s\" must "
+"be lower case"
+msgstr ""
+
+#, fuzzy, c-format
+#| msgid "%s%s: error creating \"%s\" => \"%s\""
+msgid "%s%s: error creating \"%s.%s\" => \"%s\""
 msgstr "%s%s: 作成中のエラー \"%s\" => \"%s\""
 
 #, c-format

commit 1ff001994cdfc1533bb1af965c8fe5685c951f45

irc: fix creation of irc.msgbuffer option without a server name
The regression was introduced by commit
1b669cd13cd543e80868dac17c2c4a6ad7bf4432, which allowed a server name with
upper case but rejected a name or alias with upper case.

This commit fixed the creation of the option when the server name is not given,
so this command works again:

  /set irc.msgbuffer.whois current

src/plugins/irc/irc-config.c
@@ -1577,7 +1577,8 @@ irc_config_msgbuffer_create_option_cb (const void *pointer, void *data,
                                        const char *option_name, const char *value)
 {
     struct t_config_option *ptr_option;
-    char *pos, *name_lower;
+    const char *pos;
+    char *name_lower;
     int rc;
 
     /* make C compiler happy */
@@ -1605,36 +1606,33 @@ irc_config_msgbuffer_create_option_cb (const void *pointer, void *data,
             if (value)
             {
                 pos = strrchr (option_name, '.');
-                if (pos)
+                pos = (pos) ? pos + 1 : option_name;
+                name_lower = weechat_string_tolower (pos);
+                if (name_lower && (strcmp (pos, name_lower) == 0))
                 {
-                    pos++;
-                    name_lower = weechat_string_tolower (pos);
-                    if (name_lower && (strcmp (pos, name_lower) == 0))
-                    {
-                        ptr_option = weechat_config_new_option (
-                            config_file, section,
-                            option_name, "enum",
-                            _("buffer used to display message received from IRC "
-                              "server"),
-                            "weechat|server|current|private", 0, 0, value, value, 0,
-                            NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-                        rc = (ptr_option) ?
-                            WEECHAT_CONFIG_OPTION_SET_OK_SAME_VALUE :
-                            WEECHAT_CONFIG_OPTION_SET_ERROR;
-                    }
-                    else
-                    {
-                        weechat_printf (
-                            NULL,
-                            _("%s%s: error: invalid option \"%s.%s\", the command "
-                              "name or alias \"%s\" must be lower case"),
-                            weechat_prefix ("error"), IRC_PLUGIN_NAME,
-                            "irc.msgbuffer", option_name, pos);
-                        free (name_lower);
-                        return rc;
-                    }
-                    free (name_lower);
+                    ptr_option = weechat_config_new_option (
+                        config_file, section,
+                        option_name, "enum",
+                        _("buffer used to display message received from IRC "
+                          "server"),
+                        "weechat|server|current|private", 0, 0, value, value, 0,
+                        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+                    rc = (ptr_option) ?
+                        WEECHAT_CONFIG_OPTION_SET_OK_SAME_VALUE :
+                        WEECHAT_CONFIG_OPTION_SET_ERROR;
                 }
+                else
+                {
+                    weechat_printf (
+                        NULL,
+                        _("%s%s: error: invalid option \"%s.%s\", the command "
+                          "name or alias \"%s\" must be lower case"),
+                        weechat_prefix ("error"), IRC_PLUGIN_NAME,
+                        "irc.msgbuffer", option_name, pos);
+                    free (name_lower);
+                    return rc;
+                }
+                free (name_lower);
             }
             else
                 rc = WEECHAT_CONFIG_OPTION_SET_OK_SAME_VALUE;