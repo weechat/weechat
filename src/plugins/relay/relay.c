fd88c9a45b821349abe94b2337f297be80d95921
Sébastien Helleu <flashcode@flashtux.org>
2026-04-28 21:19:10 +0200

    relay: remove protocols from the plugin description

--- src/plugins/relay/relay.c
+++ src/plugins/relay/relay.c
@@ -42,8 +42,7 @@
 
 
 WEECHAT_PLUGIN_NAME(RELAY_PLUGIN_NAME);
-WEECHAT_PLUGIN_DESCRIPTION(N_("Relay WeeChat data to remote application "
-                              "(irc/weechat protocols)"));
+WEECHAT_PLUGIN_DESCRIPTION(N_("Relay WeeChat data to remote applications"));
 WEECHAT_PLUGIN_AUTHOR("Sébastien Helleu <flashcode@flashtux.org>");
 WEECHAT_PLUGIN_VERSION(WEECHAT_VERSION);
 WEECHAT_PLUGIN_LICENSE(WEECHAT_LICENSE);
