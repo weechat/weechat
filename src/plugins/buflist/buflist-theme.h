80e14b28a5a13428941849e8be27d5ef5999b672
Sébastien Helleu <flashcode@flashtux.org>
2026-07-04 17:50:25 +0200

    buflist: contribute "light" theme overrides
    Add buflist-theme.{c,h} with the buflist plugin contribution to the
    built-in "light" theme: 5 overrides on buflist.format.* options
    (buffer_current, hotlist_low, hotlist_message, lag, number) tuned for
    a light-background terminal. Each target is a "string|themable" option
    holding an evaluated format expression with embedded ${color:...}
    references.
    
    Follows the same pattern as the irc and fset contributions: a
    NULL-terminated 2D string table consumed by a tiny local register
    helper that builds a hashtable and calls weechat_theme_register;
    buflist_theme_init() is called once from weechat_plugin_init after
    buflist_config_init / buflist_config_read.
    
    Default option values are NOT changed.

---
+++ src/plugins/buflist/buflist-theme.h
@@ -0,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Sébastien Helleu <flashcode@flashtux.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * This file is part of WeeChat, the extensible chat client.
+ *
+ * WeeChat is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WeeChat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WeeChat.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef WEECHAT_PLUGIN_BUFLIST_THEME_H
+#define WEECHAT_PLUGIN_BUFLIST_THEME_H
+
+extern void buflist_theme_init (void);
+
+#endif /* WEECHAT_PLUGIN_BUFLIST_THEME_H */
